38b6f50cd18feeaf4ea170161a4ea06c7aed1e34
Roman <11-roman@mail.ru>
2023-12-16 19:19:42 +0300

    init file

--- my_code/init.py
+++ my_code/init.py
@@ -1,22 +1,8 @@
 from tkinter import *
 
-count = 0
+root = Tk ()
+root.title ('')
+root.geometry ('350x350+200+200')
+root.config (bg = '')
 
-
-def click():
-    global count
-    count += 1
-    counter.config(text=count)
-
-root = Tk()
-root.title('Кликер')
-root.geometry('250x250+200+200')
-root.config(bg='black')
-
-counter = Label(root, text='0', font=('Arial 55'), bg='black', fg='white')
-counter.pack()
-
-clicker = Button(root, text='Нажми', font=('Arial 20'), command=click)
-clicker.pack()
-
-root.mainloop()
+root.mainloop ()
