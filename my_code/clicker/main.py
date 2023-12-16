09e353620081b515636039cb8af601b926d67d5f
Roman <11-roman@mail.ru>
2023-12-16 21:50:03 +0300

    дописал функцию, чтобы кликер еще отображал сотни кликов

--- my_code/clicker/main.py
+++ my_code/clicker/main.py
@@ -1,12 +1,18 @@
 from tkinter import *
+from math import floor
 
 count = 0
+count_100 = 0
 
 
 def click():
     global count
+    global count_100
     count += 1
     counter.config(text=count)
+    count_100 = count / 100
+    counter_100.config(text=floor(count_100))
+
 
 root = Tk()
 root.title('Кликер')
@@ -18,5 +24,8 @@ counter.pack()
 
 clicker = Button(root, text='Нажми', font=('Arial 20'), command=click)
 clicker.pack()
+
+counter_100 = Label(root, text='0', font=('Arial 55'), bg='black', fg='white')
+counter_100.pack()
 
 root.mainloop()
