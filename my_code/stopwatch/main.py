96cf4238b74f45ab44a44334568de7ed14bedf4d
Roman <11-roman@mail.ru>
2023-12-17 10:13:30 +0300

    написал функцию по параметрам самого секундомера + функцию по запуску секундомера

--- my_code/stopwatch/main.py
+++ my_code/stopwatch/main.py
@@ -1,11 +1,23 @@
 from tkinter import *
 from datetime import *
 
+temp = 0
+after_id = ''
 
-def know_time():
-    now = datetime.now()
-    timing = now.strftime('%H:%M:%S')
-    times.config(text=timing)
+
+# функция, которая берет начало отсчета от 0 и меняет наши параметры на 1 с частотой в 1 секунду
+def tick():
+    global temp, after_id
+    after_id = root.after(1000, tick)  # after - запускает действие через количество миллисекунд. 1000 = 1 секунда
+    f_temp = datetime.fromtimestamp(temp).strftime('%M:%S')  # fromtimestamp - начало точки отсчета, temp = 0
+    label_1.config(text=str(f_temp))
+    temp += 1
+
+
+def start_tick():
+    btn_start.pack_forget()  # убираем размещение нашей кнопки - Старт, на экране
+    btn_stop.pack()  # размещаем кнопку - Стоп
+    tick()  # вызываем функцию для запуска секундомера
 
 
 root = Tk()
@@ -16,23 +28,11 @@ root.config(bg='black')
 label_1 = Label(root, text='00:00', bg='black', fg='white', font=('Arial 30'))
 label_1.pack()
 
-btn_start = Button(root, text='Старт', bg='black', fg='white', font=('Arial 20'), width=15)
+btn_start = Button(root, text='Старт', bg='black', fg='white', font=('Arial 20'), width=15, command=start_tick)
 btn_start.pack()
 
 btn_stop = Button(root, text='Стоп', bg='black', fg='white', font=('Arial 20'), width=15)
-btn_stop.pack()
-
-btn_continie = Button(root, text='Продолжить', bg='black', fg='white', font=('Arial 20'), width=15)
-btn_continie.pack()
-
+btn_continue = Button(root, text='Продолжить', bg='black', fg='white', font=('Arial 20'), width=15)
 btn_reset = Button(root, text='Сбросить', bg='black', fg='white', font=('Arial 20'), width=15)
-btn_reset.pack()
-# ----------------------------------- Время сейчас ----------------------------
-
-times = Label(root, text='0', bg='black', fg='white', font=('Arial 50'))
-times.pack()
-
-time_button = Button(root, text='Узнать время', bg='black', fg='white', font=('Arial 20'), command=know_time)
-time_button.pack()
 
 root.mainloop()
