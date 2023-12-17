21133fd905674928efe10796aa7c4961024ce1a8
Roman <11-roman@mail.ru>
2023-12-17 19:25:27 +0300

    поменял начальное расположение кнопки, упростил функцию

--- my_code/ random_clicker/main.py
+++ my_code/ random_clicker/main.py
@@ -5,10 +5,8 @@ click = 0
 
 
 def running_btn():
-    x_random = r.randint(10, 400)
-    y_random = r.randint(10, 400)
     global click
-    btn_click.place(x=x_random, y=y_random)
+    btn_click.place(x=r.randint(10, 400), y=r.randint(10, 400))
     click += 1
     clicker.config(text=click)
 
@@ -19,7 +17,7 @@ root.geometry('600x600+100+100')
 root.config(bg='pink')
 
 btn_click = Button(root, text='Кликай', bg='grey', fg='white', font='Arial 20', command=running_btn)
-btn_click.place(x=50, y=100)
+btn_click.place(x=r.randint(10, 450), y=r.randint(10, 450))
 
 clicker = Label(root, text='0', bg='pink', fg='black', font='Arial 30')
 clicker.pack(expand=1)
