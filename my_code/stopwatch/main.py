c80df01987a876b27c3fe2b219e6bd91b4593de3
Roman <11-roman@mail.ru>
2023-12-17 07:51:21 +0300

    написал кнопки и лейбл для секундомера

--- my_code/stopwatch/main.py
+++ my_code/stopwatch/main.py
@@ -10,8 +10,24 @@ def know_time():
 
 root = Tk()
 root.title('')
-root.geometry('450x180+200+200')
+root.geometry('450x480+200+200')
 root.config(bg='black')
+
+label_1 = Label(root, text='00:00', bg='black', fg='white', font=('Arial 30'))
+label_1.pack()
+
+btn_start = Button(root, text='Старт', bg='black', fg='white', font=('Arial 20'), width=15)
+btn_start.pack()
+
+btn_stop = Button(root, text='Стоп', bg='black', fg='white', font=('Arial 20'), width=15)
+btn_stop.pack()
+
+btn_continie = Button(root, text='Продолжить', bg='black', fg='white', font=('Arial 20'), width=15)
+btn_continie.pack()
+
+btn_reset = Button(root, text='Сбросить', bg='black', fg='white', font=('Arial 20'), width=15)
+btn_reset.pack()
+# ----------------------------------- Время сейчас ----------------------------
 
 times = Label(root, text='0', bg='black', fg='white', font=('Arial 50'))
 times.pack()
