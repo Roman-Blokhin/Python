3974c361c2960f83fd851ae2c8ad737951485847
Roman <11-roman@mail.ru>
2023-12-17 20:01:17 +0300

    добавил контейнер, на котором разместил текстовое поле

--- my_code/notebook/main.py
+++ my_code/notebook/main.py
@@ -1,10 +1,28 @@
 from tkinter import *
 
+# ---------------------------------- ФУНКЦИИ ----------------------------------
+
+
+
+# ---------------------------------- ОКНО ----------------------------------
+
 root = Tk ()
 root.title ('Блокнот')
 root.geometry ('750x550+200+200')
 root.config (bg = 'white')
 root.resizable(width=True, height=True)
 root.iconbitmap(default='logo.ico')
+
+# ---------------------------------- КОНТЕЙНЕРЫ ----------------------------------
+
+frame_block = Frame(root)
+frame_block.pack(fill=BOTH, expand=1)
+
+# ---------------------------------- ТЕКСТОВОЕ ПОЛЕ ----------------------------------
+
+f_text = Text(frame_block, bg='white', fg='black', font=('Comic Sans MS', 14))
+f_text.pack(fill=BOTH, expand=1)
+
+# ---------------------------------- ВАЖНОЕ ----------------------------------
 
 root.mainloop ()
