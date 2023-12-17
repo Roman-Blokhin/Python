2bfedecfda3de876b8b2b7e7c7538f11bf008176
Roman <11-roman@mail.ru>
2023-12-17 19:18:31 +0300

    программка в которой кнопка убегает от пользователя при нажатии, а кликер считает, сколько раз кнопка была нажата

---
+++ my_code/ random_clicker/main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import random as r
+
+click = 0
+
+
+def running_btn():
+    x_random = r.randint(10, 400)
+    y_random = r.randint(10, 400)
+    global click
+    btn_click.place(x=x_random, y=y_random)
+    click += 1
+    clicker.config(text=click)
+
+
+root = Tk()
+root.title('Бегающая кнопка')
+root.geometry('600x600+100+100')
+root.config(bg='pink')
+
+btn_click = Button(root, text='Кликай', bg='grey', fg='white', font='Arial 20', command=running_btn)
+btn_click.place(x=50, y=100)
+
+clicker = Label(root, text='0', bg='pink', fg='black', font='Arial 30')
+clicker.pack(expand=1)
+
+root.mainloop()
