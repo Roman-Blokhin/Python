86450a5c01d9b8f6e74a09926f3cf29fe5bd065e
Roman <11-roman@mail.ru>
2023-12-21 15:56:06 +0300

    написал функцию - toplevel, создает окно. Добавил параметры и лейбл, а также .grab_set(), чтобы нельзя было выйти из программы, если дочернее окно открыто

--- lessons/tkinter/les_toplevel.py
+++ lessons/tkinter/les_toplevel.py
@@ -2,11 +2,15 @@ from tkinter import *
 
 
 def open_toplevel():
-    pass
+    win = Toplevel()  # виджет, который создает новое окно поверх первого, с его же настройками
+    win.geometry('200x200+700+400')
+    win.grab_set()  # не позволяет закрывать программу, если открыто новое окно
+    l1 = Label(win, text='Toplevel', font='Arial 15 bold', fg='brown')
+    l1.pack()
 
 
 root = Tk()
-root.title('')
+root.title('Test')
 root.geometry('350x350+200+200')
 root.config(bg='')
 root.resizable(width=False, height=False)
