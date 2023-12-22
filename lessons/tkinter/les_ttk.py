78b9024a4f937a53b6b9d93aa8acd2ba265bfce8
Roman <11-roman@mail.ru>
2023-12-22 14:41:14 +0300

    рассмотрели вариант применять стиль ко всем виджетам, либо к какому-то конкретному

--- lessons/tkinter/les_ttk.py
+++ lessons/tkinter/les_ttk.py
@@ -2,19 +2,19 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk ()
-root.title ('')
-root.geometry ('350x350+200+200')
-root.config (bg = '')
+root = Tk()
+root.title('')
+root.geometry('350x350+200+200')
+root.config(bg='')
 root.resizable(width=False, height=False)
 
 # можно изменять стили объектов
 s = ttk.Style()  # присвоили объект классу Style
 print(s.theme_names())  # выводит в консоль стили, которые есть
 print(s.theme_use())  # показывает, какой стиль у меня
-print(s.theme_use('classic'))  # позволяет выбрать стиль самостоятельно, повлияет на все объекты ttk
-
-
+print(s.theme_use('classic'))  # позволяет выбрать стиль самостоятельно
+#s.configure('.', foreground='red')  # '.' - говорит, что изменение стиля повлияет на все объекты ttk
+s.configure('TButton', foreground='blue')  # 'T...' - T обязательно ставим, если нужно применить стиль к опр. виджету
 
 # стандартная кнопка
 Button(root, text='Один', padx=10).pack()
@@ -22,4 +22,7 @@ Button(root, text='Один', padx=10).pack()
 # кнопка под стиль ОС
 ttk.Button(root, text='Два', width=20).pack()
 
-root.mainloop ()
+Entry(root).pack()
+ttk.Entry(root).pack()
+
+root.mainloop()
