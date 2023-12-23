856b4cd7b1ca144b5bda4009754ebc20a615db3e
Roman <11-roman@mail.ru>
2023-12-23 12:57:12 +0300

    подключил левую кнопку мыши, создал кнопку для сохранения

--- my_code/paint/main.py
+++ my_code/paint/main.py
@@ -3,15 +3,21 @@ from tkinter import messagebox
 import PIL
 from PIL import Image, ImageDraw
 
-root = Tk ()
-root.title ('Paint')
-root.config (bg = '')
+root = Tk()
+root.title('Paint')
+
+root.config(bg='')
 root.resizable(width=False, height=False)
 
-cv = Canvas(root, width=1280, height=720, bg='white')  # 1. создали канву - холст для рисования
+cv = Canvas(root, width=800, height=500, bg='white')  # 1. создали канву - холст для рисования
 
-image_1 = PIL.Image.new('RGB', (1280, 720), 'white')  # 2. переменная с параметрами изображения(место для рисунка)
+image_1 = PIL.Image.new('RGB', (800, 500), 'white')  # 2. переменная с параметрами изображения(место для рисунка)
 draw = PIL.ImageDraw.Draw(image_1)  # 3. инструмент для рисования
 
+cv.bind('<B1-Motion>')  # подключили левую кнопку для использования, команды пока нет
+cv.pack(expand=1, fill=BOTH)
 
-root.mainloop ()
+btn_save = Button(root, text='Сохранить', bg='black', fg='white', font=('Comic Sans MS', 30))
+btn_save.pack()
+
+root.mainloop()
