574c48abf1be6a93cbb0b855e58975d026aa19f5
Roman <11-roman@mail.ru>
2023-12-23 20:48:13 +0300

    прописал функцию для отображения линии на холсте, подключил к левой кнопки мыши

--- my_code/paint/main.py
+++ my_code/paint/main.py
@@ -3,6 +3,14 @@ from tkinter import messagebox
 import PIL
 from PIL import Image, ImageDraw
 
+
+def activate_point(event):
+    x1, y1 = (event.x - 2), (event.y - 2)  # указываем толщину линий
+    x2, y2 = (event.x + 2), (event.y + 2)  # указываем толщину линий
+    cv.create_line(x1, y1, x2, y2, fill='black', width=5)  # линия на холсте
+    draw.line((x1, y1, x2, y2), fill='black', width=5)  # линия на холсте для сохранения
+
+
 root = Tk()
 root.title('Paint')
 
@@ -14,7 +22,7 @@ cv = Canvas(root, width=800, height=500, bg='white')  # 1. создали кан
 image_1 = PIL.Image.new('RGB', (800, 500), 'white')  # 2. переменная с параметрами изображения(место для рисунка)
 draw = PIL.ImageDraw.Draw(image_1)  # 3. инструмент для рисования
 
-cv.bind('<B1-Motion>')  # подключили левую кнопку для использования, команды пока нет
+cv.bind('<B1-Motion>', activate_point)  # подключили левую кнопку для использования, команды пока нет
 cv.pack(expand=1, fill=BOTH)
 
 btn_save = Button(root, text='Сохранить', bg='black', fg='white', font=('Comic Sans MS', 30))
