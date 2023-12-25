0aa8659f5821aefcb95ea77f99dda3ed01536330
Roman <11-roman@mail.ru>
2023-12-25 20:05:21 +0300

    дописал функцию перевода с английского на русский

--- my_code/translater/main.py
+++ my_code/translater/main.py
@@ -1,13 +1,20 @@
 from tkinter import *
 from googletrans import Translator
 
+
+def trans():
+    text = text_field_1.get('1.0', END)
+    cmmnd = tranlator.translate(text, dest='en')
+    text_field_1.delete('1.0', END)
+    text_field_2.insert('1.0', cmmnd.text)
+
 root = Tk ()
 root.title ('Переводчик')
 root.geometry ('500x500+200+200')
 root['bg'] = 'black'
 root.resizable(width=False, height=False)
 
-tranlator = Translator
+tranlator = Translator()
 
 lbl_1 = Label(root, fg='white', bg='black', font='Arial 15 bold', text='Введите текст:')
 lbl_1.pack()
@@ -15,7 +22,7 @@ lbl_1.pack()
 text_field_1 = Text(root, bg='white', fg='black', font='Arial 15 bold', width=35, height=5)
 text_field_1.pack()
 
-btn_translate = Button(root, text='Перевести')
+btn_translate = Button(root, text='Перевести', command=trans)
 btn_translate.pack()
 
 text_field_2 = Text(root, bg='white', fg='black', font='Arial 15 bold', width=35, height=5)
