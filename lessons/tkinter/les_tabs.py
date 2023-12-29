63972371a0934ae14fb328e15ff10a149483a9ab
Roman <11-roman@mail.ru>
2023-12-29 17:11:44 +0300

    добавил лейблы на вкладки для наглядности

--- lessons/tkinter/les_tabs.py
+++ lessons/tkinter/les_tabs.py
@@ -15,6 +15,12 @@ tab2 = ttk.Frame(tab_control)
 tab_control.add(tab1, text='Первая')  # размещаем наши фреймы на панели вкладок
 tab_control.add(tab2, text='Вторая')
 
+lbl_1 = Label(tab1, text='Вот это че за новая страница')
+lbl_1.pack()
+
+lbl_2 = Label(tab2, text='Крутяк, новая вкладка')
+lbl_2.pack()
+
 tab_control.pack(expand=1, fill=BOTH)
 
 root.mainloop ()
