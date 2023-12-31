6a1c2615a4d04b748167e81bc65b938c03c48203
Roman <11-roman@mail.ru>
2023-12-31 09:10:05 +0300

    создали меню

--- lessons/tkinter/les_context_menu.py
+++ lessons/tkinter/les_context_menu.py
@@ -1,9 +1,21 @@
 from tkinter import *
 
 
+# ------------------------- Функции ------------------------
+
 def popup():
     pass
 
+
+def circle():
+    pass
+
+
+def square():
+    pass
+
+
+# ------------------------- Окно ------------------------
 
 root = Tk()
 root.title('Контекстное меню')
@@ -11,9 +23,20 @@ root.geometry('350x350+200+200')
 root.config(bg='')
 root.resizable(False, False)
 
+# ------------------------- Холст и клавиша мыши ------------------------
+
 c = Canvas(root, width=300, height=300, bg='white')  # создали холст для контекстного меню
 c.pack()
 
 c.bind('<Button-3>', popup)  # забиндили ПКМ, подключили функцию
+
+# ------------------------- Меню ------------------------
+
+popup_menu = Menu(tearoff=0)
+
+popup_menu.add_command(label='Круг', command=circle)
+popup_menu.add_command(label='Квадрат', command=square)
+
+# ------------------------- Важное ------------------------
 
 root.mainloop()
