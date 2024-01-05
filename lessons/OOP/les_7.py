6214af983cb47944fd43f03839501fb982adb317
Roman <11-roman@mail.ru>
2024-01-05 16:08:23 +0300

    7 урок по ООП, оптимизировали код, используя метод в методе

--- lessons/OOP/les_7.py
+++ lessons/OOP/les_7.py
@@ -3,16 +3,18 @@
 class Point:
 
     def __init__(self, coord_x=0, coord_y=0):
-        self.x = coord_x
-        self.y = coord_y
+        self.move_to(coord_x, coord_y)
 
     def move_to(self, new_x, new_y):  # метод будет менять значение точки
         self.x = new_x
         self.y = new_y
 
     def go_home(self):  # возвращает к нулевым координатам
-        self.x = 0
-        self.y = 0
+        self.move_to(0, 0)  # мы заменили замену координат, прописав метод внутри метода
+
+    def print_point(self):  # красиво выводит наши координаты
+        print(f'Точка {self}, с координатами ({self.x}, {self.y})')
+
 
 p1 = Point(3, 4)
 p2 = Point(-23, 84)
