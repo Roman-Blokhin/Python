03b2d1bd09b9db6937319c9ef39bf502f1e9c834
Roman <11-roman@mail.ru>
2024-01-09 21:16:13 +0300

    10 урок по ООП, прописал делитер для нашего свойства property

--- lessons/OOP/les_10.py
+++ lessons/OOP/les_10.py
@@ -6,18 +6,29 @@ class BankAccount:
         self.__balance = balance  # 1. сделали атрибут приватным, напрямую не обратиться
 
     def get_balance(self):  # 2. обращаемся к приватному атрибуту через метод
+        print('get')
         return self.__balance
 
     def set_balance(self, value):  # 3. устанавливаем новое значение приватному атрибуту
+        print('set')
         if not isinstance(value, (int, float)):  # 4. проверка, принадлежит ли новое значение к числу или флоат
             raise ValueError ('Недопустимое значение')
         self.__balance = value
 
+    def delete_balance(self):  # 6. добавил метод удаления и прописал делитер в проперти
+        print('del')
+        del self.__balance
+
     # 5. мы прописали свойство, которое помогает нам использовать геттер и сеттер для вывода баланса и его изменения
-    bal = property(fget=get_balance, fset=set_balance)
+    bal = property(fget=get_balance, fset=set_balance, fdel=delete_balance)
 
 
 a = BankAccount('Roman', 100)
 print('начальный баланс:', a.get_balance())
-a.set_balance('200')
+# a.set_balance('200')
 print('измененный баланс:', a.get_balance())
+
+s = BankAccount('Rob', 100)
+print(s.bal)
+s.bal = 800
+print(s.bal)
