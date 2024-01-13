c016f439d5b8e33243693aa3f0a6530060e90248
Roman <11-roman@mail.ru>
2024-01-13 17:35:56 +0300

    12 урок по ООП, прописали условие для записи значения в переменную

--- lessons/OOP/les_12.py
+++ lessons/OOP/les_12.py
@@ -3,7 +3,11 @@
 class Square:
     def __init__(self, side):
         self.side = side
+        self.__area = None  # 2. создали приватную переменную для определения площади. По умолчанию = None
 
-    @property  # делаем из метода свойство, чтобы мы вызывали его ни как метод, а как атрибут - a.area
+    @property  # 1. делаем из метода свойство, чтобы мы вызывали его ни как метод, а как атрибут - a.area
     def area(self):
-        return self.side**2
+        if self.__area is None:  # 3. проверка, если переменная пустая, то в нее записывается 1 раз значение площади
+            print('create area')
+            self.__area = self.side**2
+        return self.__area
