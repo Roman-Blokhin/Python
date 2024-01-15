772d722e623d4a3ef000eea2c2ef91ee36b04a5b
Roman <11-roman@mail.ru>
2024-01-15 14:37:06 +0300

    14 урок по ООП, изменение значений внутри метода и внутри самого класса

--- lessons/OOP/les_14.py
+++ lessons/OOP/les_14.py
@@ -23,6 +23,13 @@ class DepartmentIt:
     def info_staticmethod():  # 5. мы можем обратиться к переменным через свойство staticmethod - нет атрибутов
         print(DepartmentIt.PYTHON, DepartmentIt.JAVA, DepartmentIt.HTML)
 
+    # 6. смоделируем ситуацию, когда нам нужно нанять разработчика
+    # так как мы обращаемся к переменной - DepartmentIt, то не создается локальное значение PYTHON, а меняется в классе
+    # при создании нового экземпляра у переменной будет уже измененное значение, а не начальное
+    def person(self):
+        DepartmentIt.PYTHON += 1
+        print('Python:', DepartmentIt.PYTHON)
+
 
 it_1 = DepartmentIt()
 
@@ -30,4 +37,11 @@ it_1.info()  # 1
 it_1.info_2()  # 2
 it_1.info_classmethod()  # 4
 it_1.info_staticmethod()  # 5
+
+# 6. смотрим, как меняются переменные в самом классе
+it_2 = DepartmentIt()
+it_2.info()
+it_2.person()
+it_2.info()
+
 it_1.info_property()  # 3
