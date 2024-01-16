afeaa401f967beed725827e1f7e2838f0eb24b29
Roman <11-roman@mail.ru>
2024-01-16 22:18:00 +0300

    16 урок по ООП, магический метод __repr__ - отображение объекта со стороны разработчика

---
+++ lessons/OOP/les_16.py
@@ -0,0 +1,8 @@
+# ООП 15 Магические методы. Методы __str__ и __repr__. (Dunder methods)
+
+class Lion:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):  # 1. отвечает за то, как отображается объект в системе, для разработчиков
+        return f"Lion - {self.name}"
