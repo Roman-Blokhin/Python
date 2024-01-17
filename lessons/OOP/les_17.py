05bb9e696c6d5977d13098610df0f0180ca82a61
Roman <11-roman@mail.ru>
2024-01-17 22:51:19 +0300

    17 урок по ООП, магический метод __abs__ - выводит положительное число вместо отрицательного

--- lessons/OOP/les_17.py
+++ lessons/OOP/les_17.py
@@ -12,3 +12,18 @@ class User:
 b = User('roman', 'hero')
 b.__len__()  # можно вызвать так метод
 len(b)  # а можно так
+
+
+class Otrezok:
+    def __init__(self, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+
+    def __len__(self):  # 3. автоматически подтягивает встроенную функцию, когда прописан сам метод в классе
+        return abs(self)  # self.__abs__()
+
+    def __abs__(self):  # 2. прописываем метод, который убирает минус у числа
+        return abs(self.x2 - self.x1)
+
+q = Otrezok(90, 6)
+len(q)  # выводит положительное число
