0c13512d6d01a1e2073db93652cd71138aa3aaca
Roman <11-roman@mail.ru>
2024-01-19 14:19:35 +0300

    15 урок по ООП, практика по свойству property. прописал проверки для сохранения данных

--- lessons/OOP/les_15.py
+++ lessons/OOP/les_15.py
@@ -1,15 +1,34 @@
 # Практика по методам и свойствам (property)
 
+from string import digits
+
+
 class User:
     def __init__(self, login, password):
         self.login = login
         self.__password = password  # 1. аргумент должен быть защищенным/приватным
 
+    @staticmethod  # 5. проверка, содержит ли пароль цифры
+    def is_include_digit(password):
+        for digit in digits:
+            if digit in password:
+                return True
+        return False
+
     @property  # 2. делаем свойство - геттер
     def password(self):
+        print('get')
         return self.__password
 
-    @password.setter
+    @password.setter  # 3. делаем свойство - сеттер
     def password(self, value):
+        print('set')
+        if not isinstance(value, str):  # 4. проверяем принадлежность данных к определенному типу
+            raise TypeError('Пароль должен быть строкой')
+        if len(value) < 3:
+            raise ValueError('Пароль не может быть меньше 3 символов')
+        if len(value) > 10:
+            raise ValueError('Пароль не может быть не больше 10 символов')
+        if not User.is_include_digit(value):
+            raise ValueError('Пароль должен содержать цифры')
         self.__password = value
-
