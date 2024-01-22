47f0d204794557723fdf4ac2cfeaa88fcbfe8812
Roman <11-roman@mail.ru>
2024-01-22 22:13:23 +0300

    18 урок по ООП, магический метод __radd__ для перемены мест слогаемых

--- lessons/OOP/les_18.py
+++ lessons/OOP/les_18.py
@@ -13,3 +13,6 @@ class BankAccount:
         if isinstance(other, BankAccount):  # 3. позволяет сложить объект с объектом и получить общи баланс
             return self.balance + other.balance
         raise NotImplemented  # 4. если проверки не проходят, мы не можем сделать вычисление
+
+    def __radd__(self, other):  # 5. позволяет избежать ошибки, когда число складываем с объектом self - (12 + w)
+        return self + other  # 5.1 меняет местами объекты для корректного сложения
