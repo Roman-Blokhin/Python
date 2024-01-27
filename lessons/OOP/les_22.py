eda319e31a52065d5b68d87988e4fa7e5d882c00
Roman <11-roman@mail.ru>
2024-01-27 15:42:39 +0300

    22 урок по ООП, полиморфизм. Доработали второй удобный метод с помощью магического метода __str__

--- lessons/OOP/les_22.py
+++ lessons/OOP/les_22.py
@@ -9,6 +9,8 @@ class Rect:
     def get_area(self):
         return self.a * self.b
 
+    def __str__(self):
+        return f'Rectangle {self.a}x{self.b} ='
 
 class Square:
     def __init__(self, a):
@@ -17,6 +19,8 @@ class Square:
     def get_area(self):
         return self.a ** 2
 
+    def __str__(self):
+        return f'Square {self.a}x{self.a} ='
 
 class Circle:
     def __init__(self, r):
@@ -25,6 +29,8 @@ class Circle:
     def get_area(self):
         return 3.14 * self.r ** 2
 
+    def __str__(self):
+        return f'Circle radius {self.r} ='
 
 rect1 = Rect(3, 4)
 rect2 = Rect(30, 40)
@@ -43,6 +49,7 @@ cir2 = Circle(70)
 
 figures = [rect1, rect2, sq1, sq2, cir1, cir2]
 
+# первый неудобный метод:
 for i in figures:
     if isinstance(i, Rect):
         print(i.get_area())
@@ -50,3 +57,9 @@ for i in figures:
         print(i.get_area())
     elif isinstance(i, Circle):
         print(i.get_area())
+
+print('')
+
+# второй удобный метод:
+for i in figures:
+    print(i, i.get_area())
