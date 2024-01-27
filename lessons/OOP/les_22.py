fd668c4a032b2bcf96dd27ba963823ded1781b4b
Roman <11-roman@mail.ru>
2024-01-27 15:36:18 +0300

    22 урок по ООП, полиморфизм. Замена названий методов в каждом классе на общее

--- lessons/OOP/les_22.py
+++ lessons/OOP/les_22.py
@@ -1,11 +1,12 @@
 # ООП 22 Полиморфизм в Python. Polymorphism python
-
+# Полиморфизм - возможность обработки разных объектов одним методом по одному названию
+# Смысл в том, чтобы у всех методов В РАЗНЫХ КЛАССАХ было одно название
 class Rect:
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
-    def get_rect_area(self):
+    def get_area(self):
         return self.a * self.b
 
 
@@ -13,7 +14,7 @@ class Square:
     def __init__(self, a):
         self.a = a
 
-    def get_square_area(self):
+    def get_area(self):
         return self.a ** 2
 
 
@@ -21,31 +22,31 @@ class Circle:
     def __init__(self, r):
         self.r = r
 
-    def get_circle_area(self):
+    def get_area(self):
         return 3.14 * self.r ** 2
 
 
 rect1 = Rect(3, 4)
 rect2 = Rect(30, 40)
 
-print(rect1.get_rect_area(), rect2.get_rect_area())
+# print(rect1.get_rect_area(), rect2.get_rect_area())
 
 sq1 = Square(3)
 sq2 = Square(30)
 
-print(sq1.get_square_area(), sq2.get_square_area())
+# print(sq1.get_square_area(), sq2.get_square_area())
 
 cir1 = Circle(2)
 cir2 = Circle(70)
 
-print(sq1.get_square_area(), sq2.get_square_area())
+# print(sq1.get_square_area(), sq2.get_square_area())
 
 figures = [rect1, rect2, sq1, sq2, cir1, cir2]
 
 for i in figures:
     if isinstance(i, Rect):
-        print(i.get_rect_area())
+        print(i.get_area())
     elif isinstance(i, Square):
-        print(i.get_square_area())
+        print(i.get_area())
     elif isinstance(i, Circle):
-        print(i.get_circle_area())
+        print(i.get_area())
