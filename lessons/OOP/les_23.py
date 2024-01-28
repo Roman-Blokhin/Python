3eb02b357b035444467f8023fc72ffe2d35f9669
Roman <11-roman@mail.ru>
2024-01-28 10:09:23 +0300

    23 урок по ООП, магический метод __call__. Создали изменяющиеся аргументы суммы и длины кортежа. Есть метод average, который находит среднее значение

--- lessons/OOP/les_23.py
+++ lessons/OOP/les_23.py
@@ -3,8 +3,27 @@
 class Counter:
     def __init__(self):
         self.count = 0
+        self.summa = 0
+        self.length = 0
         print(f'New object: {self}')
 
     def __call__(self, *args, **kwargs):
         self.count += 1
+        self.summa += sum(args)  # считает сумму аргументов, прибавляя к уже имеющимся аргументам
+        self.length += len(args)  # считает количество аргументов, прибавляя к уже имеющемуся количеству аргументов
         return f'Счетчик вызван {self.count} раз'
+
+    def average(self):
+        return self.summa / self.length
+
+a = Counter()
+a(1, 2, 3, 4)
+print('Вызвана функция:', a.count)
+print('Сумма:', a.summa)
+print('Длина:', a.length)
+a(6, 90)
+print('Вызвана функция:', a.count)
+print('Сумма:', a.summa)
+print('Длина:', a.length)
+
+print(a.average())  # находим среднее значение
