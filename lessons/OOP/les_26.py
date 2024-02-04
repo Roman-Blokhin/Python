9ff51c2dc74da3ac628ed4da9a96a74f9846c1db
Roman <11-roman@mail.ru>
2024-02-04 09:46:48 +0300

    26 урок по ООП. Наследование классов. Иерархия. isinstance, issubclass

--- lessons/OOP/les_26.py
+++ lessons/OOP/les_26.py
@@ -18,6 +18,10 @@ class Teacher(Person):  # наследуем класс Person, чтобы не 
         print('Я - учитель')
 
 
+class Ortoped(Doctor):
+    pass
+
+
 # ИСПОЛЬЗУЕМ НАСЛЕДОВАНИЕ КЛАССА, ЧТОБЫ ОДИНАКОВЫЙ КОД НАХОДИЛСЯ В ОДНОМ МЕСТЕ
 a = Doctor()
 
@@ -25,10 +29,31 @@ a.iam()
 a.breathe()
 a.walking()
 
-print('------')
-
 b = Teacher()
 
 b.iam()
 b.breathe()
 b.walking()
+
+print('2. ------')
+
+# Мы можем проверить, является ли класс чьим-нибудь подклассом
+
+print(issubclass(Doctor, Person))
+print(issubclass(Teacher, Person))
+
+print('3. ------')
+
+# Мы можем проверить, принадлежит ли экземпляр к классу
+
+print(isinstance(a, Doctor))
+print(isinstance(a, Person))  # спрашиваем про родительский класс
+print(isinstance(b, Doctor))
+
+print('4. ------')
+
+# Наследование по цепочки может быть бесконечным: Person - Doctor - Ortoped
+# Последний класс будет иметь все свойства предыдущих классов
+
+c = Ortoped()
+print(isinstance(c, Person))
