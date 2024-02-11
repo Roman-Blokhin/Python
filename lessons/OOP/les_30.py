9f57b3ef4aa46a136fad0aa5936d8427ef8a1309
Roman <11-roman@mail.ru>
2024-02-11 16:07:32 +0300

    30 урок по ООП. Делегирование. info() + проверки

--- lessons/OOP/les_30.py
+++ lessons/OOP/les_30.py
@@ -18,12 +18,20 @@ d = Doctor()
 
 d.breathe()  # 1.1 для вызова будет использоваться только одна строчка
 
+# ------------------------------------------------------
 
 class User:
 
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+
+    def __str__(self):
+        return f'Person {self.name} {self.surname}'  # 3 будет self от класса Doctor, если через него вызывать
+
+    def info(self):
+        print('Parent class')
+        print(self)  # 3.1 будет self от класса Doctor, если через него вызывать
 
 
 class Settler(User):
@@ -32,9 +40,15 @@ class Settler(User):
         super().__init__(name, surname)  # 2. чтобы не было повторения кода, лучше вызывать перед новыми атрибутами
         self.age = age  # 2.1 писать после super(), чтобы значение не перезаписывалось
 
+    def __str__(self):
+        return f'Doctor {self.name} {self.surname}'
+
 
 a = User('Roman', 'Romanovich')
 b = Settler('Toy', 'Toyevich', 45)
 
 print(a.name, a.surname)
 print(b.name, b.surname, b.age)
+
+a.info()
+b.info()  # вызывет self от Doctor
