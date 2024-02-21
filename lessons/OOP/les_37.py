b68bd44c80af9fe1a2229b23b5d65039107ac359
Roman <11-roman@mail.ru>
2024-02-21 15:34:19 +0300

    37 урок по ООП. Исключения. raise - выводим только одно исключение из нескольких

--- lessons/OOP/les_37.py
+++ lessons/OOP/les_37.py
@@ -5,3 +5,11 @@ try:
 except ZeroDivisionError as err:  # 1. мы можем присваивать имя
     print('На ноль делить нельзя')
     print(f'Logging error: {repr(err)}')  # 2. можем к этому имени обращаться, чтобы понять суть ошибки
+
+# -------------------------------------
+
+try:
+    1/0
+except ZeroDivisionError as err:
+    print(f'Logging error: {repr(err)}')
+    raise TypeError('Ошибка типа') from None  # 3. если несколько исключений, но нужно вывести только 1 конкретное
