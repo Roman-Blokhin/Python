007bff6c949bc590f6cba8524e6e7a0d21b750ab
Roman <11-roman@mail.ru>
2024-02-22 22:24:09 +0300

    38 урок по ООП. Исключения. Обработка исключений через __init__ и __str__

--- lessons/OOP/les_38.py
+++ lessons/OOP/les_38.py
@@ -1,7 +1,25 @@
 # OOP 38 Пользовательские исключения в Python Custom Exception Python
 
+class MyExcept(Exception):
+    def __init__(self, *args):  # 4. передаем любое количество аргументов
+        if args:
+            self.message = args[0]  # 5. сохраняем все под индексом ноль, соответственно будет 1 аргумент только
+        else:
+            self.message = None
+
+    def __str__(self):  # 6. описываем вывод в консоли
+        if self.message:
+            return f'Message ({self.message})'
+        else:
+            return 'Message is empty'
+
+raise MyExcept('roman', 'toy', 789)
+
+# ---------------------------------------- 2 -------------------------------------------
+
 class MyException(Exception):  # 1. все наследуется от класса исключений Exception
-    pass
+    """roman is a hero"""
+
 
 try:
     raise MyException('1', '2', 'hello')  # 2. мы можем, обрабатывая исключение, передавать в него аргументы
@@ -14,3 +32,5 @@ try:
     raise MyException('1', '2', 'hello')
 except AttributeError:  # 3. не входит в class Exception, поэтому исключение не обрабатывается
     print('done')
+
+
