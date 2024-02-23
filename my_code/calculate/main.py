378c81adb3c18aa8831b34d16150670c7bf473e1
Roman <11-roman@mail.ru>
2024-02-23 21:19:09 +0300

    Создание калькулятора с графическим дизайном. Окно вывода

--- my_code/calculate/main.py
+++ my_code/calculate/main.py
@@ -14,6 +14,11 @@ root.geometry('400x400+200+200')
 root.config(bg='Grey')
 root.resizable(False, False)
 
+# ------------------------------- ОКНО ВЫВОДА -------------------------------
+
+entry = Entry(root, width=15, font=('Comic Sans MS', 15, 'bold'))
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
 # ------------------------------- ВАЖНОЕ -------------------------------
 
 root.mainloop()
