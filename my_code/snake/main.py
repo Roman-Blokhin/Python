93c716922e2ef65212c30404a58cc618d0604375
Roman <11-roman@mail.ru>
2024-02-25 10:21:14 +0300

    Pygame. Game Snake. Создание квадрата на экране

--- my_code/snake/main.py
+++ my_code/snake/main.py
@@ -1,6 +1,10 @@
 import pygame
 
 FRAME_COLOR = (0, 255, 204)  # 7. window color
+WHITE = (255, 255, 255)  # 10. rect color
+
+SIZE_BLOCK = 20  # 12. rectangle's size
+COUNT_BLOCKS = 20  # 13. сколько у нас будет квадратиков на экране
 
 size = [400, 600]  # 1. переменная с размерами для окна
 
@@ -19,4 +23,8 @@ while True:
 
     # 8. создаем заливку нашего окна
     screen.fill(FRAME_COLOR)
+
+    # 11. рисуем квадратики нашего поля(место, цвет, [расположение и размер квадрата])
+    pygame.draw.rect(screen, WHITE, [10, 20, SIZE_BLOCK, SIZE_BLOCK])
+
     pygame.display.flip()  # 9. экран переворачивается к нам лицом и мы видим результат работы
