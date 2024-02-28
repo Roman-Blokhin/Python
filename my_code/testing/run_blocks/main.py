b4a491023befe880207f9172218b13afaabb880e
Roman <11-roman@mail.ru>
2024-02-28 12:59:07 +0300

    Pygame - бегущие блоки. Нужное расстояние у блоков

--- my_code/testing/run_blocks/main.py
+++ my_code/testing/run_blocks/main.py
@@ -1,6 +1,7 @@
 # ХОЧУ СДЕЛАТЬ ПЕРВЫЙ МАКЕТ ДЛЯ ИГРЫ SPACE BATTLE, ГДЕ КОРАБЛИ ПРОТИВНИКА ЛЕТЯТ НА ТЕБЯ, А ТЕБЕ НУЖНО ИХ СБИВАТЬ
 
 import pygame
+
 pygame.init()
 
 # ---------------------- variables ------------------------
@@ -12,10 +13,10 @@ BLUE = (0, 0, 155)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-size = (400, 600)
+size = (500, 600)
 block_size = 20
-count_blocks = 15
-MARGIN = 2
+count_blocks = 10
+MARGIN = 20
 
 FPS = 60
 
@@ -34,7 +35,6 @@ font = pygame.font.SysFont('Comic Sans MS', 30)
 # ---------------------- objects ------------------------
 
 
-
 # ---------------------- cycle ------------------------
 
 while True:
@@ -46,7 +46,6 @@ while True:
 
     screen.fill(FRAME_COLOR)
 
-
     for row in range(3):
         for column in range(count_blocks):
             if (row + column) % 2 == 0:
@@ -54,10 +53,9 @@ while True:
             else:
                 color = RED
 
-            pygame.draw.rect(screen, color, [30 + column*count_blocks + MARGIN*(column+1),
-                                             30 + row*count_blocks + MARGIN*(row+1),
+            pygame.draw.rect(screen, color, [50 + column * count_blocks + MARGIN * (column + 1),
+                                             50 + row * count_blocks + MARGIN * (row + 1),
                                              block_size,
                                              block_size])
 
     pygame.display.update()
-
