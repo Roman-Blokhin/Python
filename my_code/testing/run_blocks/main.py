926de72b99b626dff49c398c076f1ec9dd5a2fe6
Roman <11-roman@mail.ru>
2024-02-28 10:02:06 +0300

    Pygame - бегущие блоки. Создание квадратов, колонок и рядов

--- my_code/testing/run_blocks/main.py
+++ my_code/testing/run_blocks/main.py
@@ -13,16 +13,27 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 size = (400, 600)
+block_size = 20
+count_blocks = 15
+MARGIN = 2
 
 FPS = 60
+
+direct_x = 1
+direct_y = 1
 
 # ---------------------- window ------------------------
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Running bloks')
+pygame.display.set_caption('Running blocks')
 img = pygame.image.load('space_logo.png')
 pygame.display.set_icon(img)
 clock = pygame.time.Clock()
+font = pygame.font.SysFont('Comic Sans MS', 30)
+
+# ---------------------- objects ------------------------
+
+
 
 # ---------------------- cycle ------------------------
 
@@ -34,6 +45,19 @@ while True:
     clock.tick(FPS)
 
     screen.fill(FRAME_COLOR)
+
+
+    for row in range(3):
+        for column in range(count_blocks):
+            if (row + column) % 2 == 0:
+                color = WHITE
+            else:
+                color = RED
+
+            pygame.draw.rect(screen, color, [30 + column*count_blocks + MARGIN*(column+1),
+                                             30 + row*count_blocks + MARGIN*(row+1),
+                                             block_size,
+                                             block_size])
 
     pygame.display.update()
 
