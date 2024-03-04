f101e02b746da9f10388fa2aa0ffc75fa240d862
Roman <11-roman@mail.ru>
2024-03-04 21:19:09 +0300

    5. Pygame. Игра Крестики-нолики. Создал основное окно, добавил цикл, массив, размеры

---
+++ my_code/tic_tac_toe/main.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+
+# ------------------------------------------ VARIABLES ------------------------------------------
+
+FRAME_COLOR = (100, 100, 100)
+RED = (155, 0, 0)
+GREEN = (0, 155, 0)
+BLUE = (0, 0, 155)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+block_size = 100
+margin = 15
+width = height = block_size * 3 + margin * 4
+size = (width, height)
+
+FPS = 60
+
+# ------------------------------------------ WINDOW ------------------------------------------
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Крестики нолики')
+img = pygame.image.load('logo.png')
+pygame.display.set_icon(img)
+clock = pygame.time.Clock()
+
+mas = [[0]*3 for i in range(3)]
+
+# ------------------------------------------ CYCLE ------------------------------------------
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    clock.tick(FPS)
+
+    screen.fill(FRAME_COLOR)
+
+    pygame.display.update()
