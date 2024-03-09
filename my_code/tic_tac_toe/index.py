1e8d960d76d813ae82b8b6178e578453f7b25a68
Roman <11-roman@mail.ru>
2024-03-09 19:04:22 +0300

    5. Pygame. Крестики-нолики. Создание массива, обработка нажатия клавиш, получение координат нажатия

--- my_code/tic_tac_toe/index.py
+++ my_code/tic_tac_toe/index.py
@@ -19,6 +19,9 @@ margin = 15
 width = height = block_size * 3 + margin * 4
 size = (width, height)
 
+# 2. создали массив для присваивания каждому квадрату значение - 0
+mas = [[0]*3 for i in range(count_block)]
+
 FPS = 60
 
 # ------------------------------------------ WINDOW ------------------------------------------
@@ -36,13 +39,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
+        # 3. обрабатываем нажатие левой кнопки мыши
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # 4. получение координат нажатия на экране игры
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            print(x_mouse, y_mouse)
+
+
     clock.tick(FPS)
     screen.fill(FRAME_COLOR)
 
+    # 1. прописали цикл вывода квадратов игрового поля
     for row in range(count_block):
         for col in range(count_block):
-            x = col*block_size + margin*(col+1)
-            y = row*block_size + margin*(row+1)
+
+            x = col * block_size + margin * (col + 1)
+            y = row * block_size + margin * (row + 1)
             pygame.draw.rect(screen, RED, (x, y, block_size, block_size))
 
     pygame.display.update()
