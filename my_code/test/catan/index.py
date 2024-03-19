68cb77f5f1d48e6ed0206915a9f3ec11a66e6fff
Roman <11-roman@mail.ru>
2024-03-19 15:41:47 +0300

    Игра Catan. Функция выводящая список пустых элементов. Функция находящая номер элемента в массиве

--- my_code/test/catan/index.py
+++ my_code/test/catan/index.py
@@ -2,16 +2,33 @@ import random
 
 
 def pretty_print(mas):
+    print('-' * 10)
     for row in mas:
         print(*row)
+    print('-' * 10)
+
+
+def get_num_from_index(i, j):
+    return i * 4 + j + 1
+
+
+def get_empty_list(mas):
+    empty = []
+    for i in range(5):
+        for j in range(4):
+            if mas[i][j] == 0:
+                num = get_num_from_index(i, j)
+                empty.append(num)
+    return empty
 
 
 mas = [
-    [0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0]
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
 ]
 
+print(get_empty_list(mas))
 pretty_print(mas)
