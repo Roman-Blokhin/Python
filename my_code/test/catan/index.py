3c8d17f5858e38693cadfd4fa6be14c9bf895fc1
Roman <11-roman@mail.ru>
2024-03-19 15:25:41 +0300

    Игра Catan. Пустой массив

---
+++ my_code/test/catan/index.py
@@ -0,0 +1,17 @@
+import random
+
+
+def pretty_print(mas):
+    for row in mas:
+        print(*row)
+
+
+mas = [
+    [0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0]
+]
+
+pretty_print(mas)
