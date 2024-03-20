043d8ee44cea8f7dea1d4f0f51b33f098c7bb84d
Roman <11-roman@mail.ru>
2024-03-20 17:26:05 +0300

    Игра Catan. Функция выводящая индекс(координаты) по номеру ячейки

--- my_code/testing/catan/index.py
+++ my_code/testing/catan/index.py
@@ -12,9 +12,15 @@ def get_num_from_index(i, j):
     return i * 4 + j + 1
 
 
+def get_index_from_num(num):
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
+
+
 def get_empty_list(mas):
     empty = []
-    for i in range(5):
+    for i in range(4):
         for j in range(4):
             if mas[i][j] == 0:
                 num = get_num_from_index(i, j)
@@ -26,9 +32,11 @@ mas = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0],
     [0, 0, 0, 0]
 ]
 
 print(get_empty_list(mas))
+print(get_index_from_num(1))
 pretty_print(mas)
+
+# 16 = 3 red 5 green 4 sheep 2 grey 2 yellow
