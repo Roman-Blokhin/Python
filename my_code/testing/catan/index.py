019b9fac4d35d86f9d43788fb335d9ba214e94f5
Roman <11-roman@mail.ru>
2024-03-21 19:33:57 +0300

    Игра Catan. Функция, заполняющая массив числами от 1 до 5, которые будут символизировать ресурсы

--- my_code/testing/catan/index.py
+++ my_code/testing/catan/index.py
@@ -28,6 +28,19 @@ def get_empty_list(mas):
     return empty
 
 
+def insert_num_to_mas(mas, x, y):
+    if random.random <= 0.2:
+        mas [x][y] = 1
+    elif random.random <= 0.4:
+        mas [x][y] = 2
+    elif random.random <= 0.5:
+        mas [x][y] = 3
+    elif random.random <= 0.8:
+        mas [x][y] = 4
+    elif random.random <= 1:
+        mas [x][y] = 5
+
+
 mas = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -36,7 +49,7 @@ mas = [
 ]
 
 print(get_empty_list(mas))
-print(get_index_from_num(1))
+print(get_index_from_num(2))
 pretty_print(mas)
 
 # 16 = 3 red 5 green 4 sheep 2 grey 2 yellow
