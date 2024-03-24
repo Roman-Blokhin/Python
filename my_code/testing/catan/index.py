84e947329b4a2783e579a96ce2347e6f98ba28f8
Roman <11-roman@mail.ru>
2024-03-24 11:16:24 +0300

    Игра Catan. Создание констант для pygame

--- my_code/testing/catan/index.py
+++ my_code/testing/catan/index.py
@@ -3,6 +3,18 @@ import sys
 from logic import *
 pygame.init()
 
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (155, 155, 155)
+
+COUNT_BLOCK = 4
+MARGIN = 10
+WIDTH, HEIGHT = COUNT_BLOCK * 4 + MARGIN * 5
+
 mas = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -12,10 +24,11 @@ mas = [
 
 
 while is_sero_in_mas(mas):
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         pygame.quit()
-    #         sys.exit(0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
 
     input()
     empty = get_empty_list(mas)
@@ -27,6 +40,6 @@ while is_sero_in_mas(mas):
     print(get_empty_list(mas))
     pretty_print(mas)
 
-    # pygame.display.update()
+    pygame.display.update()
 
 # 16 = 3 red 5 green 4 sheep 2 grey 2 yellow
