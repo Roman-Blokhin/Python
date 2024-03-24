03cdc1ece75b3dcc134f703bbb7471983f145e79
Roman <11-roman@mail.ru>
2024-03-24 08:27:08 +0300

    Игра Catan. Создания файла logic.py и перенос всех функций

--- my_code/testing/catan/index.py
+++ my_code/testing/catan/index.py
@@ -1,57 +1,7 @@
-import random
-import sys
 import pygame
-
+import sys
+from logic import *
 pygame.init()
-
-
-def pretty_print(mas):
-    print('-' * 10)
-    for row in mas:
-        print(*row)
-    print('-' * 10)
-
-
-def get_num_from_index(i, j):
-    return i * 4 + j + 1
-
-
-def get_index_from_num(num):
-    num -= 1
-    x, y = num // 4, num % 4
-    return x, y
-
-
-def get_empty_list(mas):
-    empty = []
-    for i in range(4):
-        for j in range(4):
-            if mas[i][j] == 0:
-                num = get_num_from_index(i, j)
-                empty.append(num)
-    return empty
-
-
-def insert_num_to_mas(mas, x, y):
-    if random.random() <= 0.2:
-        mas[x][y] = 1
-    elif random.random() <= 0.4:
-        mas[x][y] = 2
-    elif random.random() <= 0.5:
-        mas[x][y] = 3
-    elif random.random() <= 0.8:
-        mas[x][y] = 4
-    elif random.random() <= 1:
-        mas[x][y] = 5
-    return mas
-
-
-def is_sero_in_mas():
-    for row in mas:
-        if 0 in row:
-            return True
-    return False
-
 
 mas = [
     [0, 0, 0, 0],
@@ -60,7 +10,8 @@ mas = [
     [0, 0, 0, 0]
 ]
 
-while is_sero_in_mas():
+
+while is_sero_in_mas(mas):
     # for event in pygame.event.get():
     #     if event.type == pygame.QUIT:
     #         pygame.quit()
