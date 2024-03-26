dfbcf73160918fe9f2e2a5118b1c71895f4637cd
Roman <11-roman@mail.ru>
2024-03-26 18:18:37 +0300

    6. Pygame. 2048. Отрисовка экрана вровень с выводом массива, чтобы не было опозданий

--- my_code/game_2048/main.py
+++ my_code/game_2048/main.py
@@ -47,6 +47,9 @@ while is_zero_in_mas(mas):
             # 14. создаем шрифт для отображения цифр на нашем игровом поле - название и размер
             font = pygame.font.SysFont('Comic Sans MS', 70)
 
+            # 15. Передвинули вывод массива в консоль перед отрисовкой игры, чтобы не было опоздания
+            pretty_print(mas)
+
             # 13. рисуем квадраты игрового поля
             for row in range(BLOCK):
                 for column in range(BLOCK):
@@ -74,9 +77,8 @@ while is_zero_in_mas(mas):
             x, y = get_index_from_number(random_num)  # 8.4 получаем координаты нашего числа
             mas = insert_2_or_4(mas, x, y)  # 8.5 присваиваем по этим координатам 2 или 4 в ячейку
             print(f'Заполнен элемент под номером: {random_num}. Координаты: {x}, {y}')
-            pretty_print(mas)
 
-    pygame.display.update()
+            pygame.display.update()  # подвинули внутрь цикла, чтобы обновление только при нажатии клавиши сразу
 
 # ----------------------------- COMMENTS ----------------------------
 # массив можно писать в таком виде: mas_2 = [[0]*4 for i in range(4)]
