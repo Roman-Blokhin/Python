ef845276cc58432e3d67bc99718d37a0e3945ec8
Roman <11-roman@mail.ru>
2024-03-27 08:33:23 +0300

    Catan. Функция отрисовки игры. Меняем код местами для корректной отрисовки

--- my_code/game_2048/main.py
+++ my_code/game_2048/main.py
@@ -2,6 +2,32 @@ from logics import *
 import pygame
 import sys
 
+
+# 17. Переносим всю отрисовку интерфейса в функцию
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_REC)
+    # 14. создаем шрифт для отображения цифр на нашем игровом поле - название и размер
+    font = pygame.font.SysFont('Comic Sans MS', 70)
+    # 15. Передвинули вывод массива в консоль перед отрисовкой игры, чтобы не было опоздания
+    pretty_print(mas)
+    # 13. рисуем квадраты игрового поля
+    for row in range(BLOCK):
+        for column in range(BLOCK):
+            # 14.1 находим значение ,которое хранится в массиве
+            value = mas[row][column]
+            # 14.2 прописываем, что будем писать и как
+            text = font.render(f'{value}', True, BLACK )  # значение, обтекание шрифта и цвет
+            w = column * SIZE_BLOCK + (column+1) * MARGIN
+            h = (row * SIZE_BLOCK + (row+1) * MARGIN) + 110
+            pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))  # 16.1 меняем цвет ячейки
+            # в соответствии со значением из словаря COLORS
+            # 14.3 условие для вывода значения на квадрате
+            if value != 0:
+                font_w, font_h = text.get_size()  # 14.4 узнаем размер текста
+                text_x = w + (SIZE_BLOCK - font_w) / 2  # 14.5 координата размещение текста в квадрате по Х
+                text_y = h + (SIZE_BLOCK - font_h) / 2  # 14.6 координата размещение текста в квадрате по У
+                screen.blit(text, (text_x, text_y))  # 14.7 размещаем текст на экране по координатам
+
 # 1. создаем двухмерный массив. он проиндексирован. у каждого эл. 2 индекса
 mas = [
     [0, 0, 0, 0],
@@ -44,6 +70,9 @@ mas[3][0] = 2
 print(get_empty_list(mas))
 pretty_print(mas)
 
+draw_interface()  # 17.2 вставляем функцию отрисовки интерфейса
+pygame.display.update()  # 17.3 обновляем экран перед циклом, сразу игра будет видна
+
 # 8. Создаем цикл игры
 while is_zero_in_mas(mas):
     for event in pygame.event.get():  # 12.1 обработка события - закрытие окна
@@ -51,34 +80,6 @@ while is_zero_in_mas(mas):
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:  # 12.2 обработка события - нажатие на любую клавишу
-            pygame.draw.rect(screen, WHITE, TITLE_REC)
-
-            # 14. создаем шрифт для отображения цифр на нашем игровом поле - название и размер
-            font = pygame.font.SysFont('Comic Sans MS', 70)
-
-            # 15. Передвинули вывод массива в консоль перед отрисовкой игры, чтобы не было опоздания
-            pretty_print(mas)
-
-            # 13. рисуем квадраты игрового поля
-            for row in range(BLOCK):
-                for column in range(BLOCK):
-                    # 14.1 находим значение ,которое хранится в массиве
-                    value = mas[row][column]
-                    # 14.2 прописываем, что будем писать и как
-                    text = font.render(f'{value}', True, BLACK )  # значение, обтекание шрифта и цвет
-
-                    w = column * SIZE_BLOCK + (column+1) * MARGIN
-                    h = (row * SIZE_BLOCK + (row+1) * MARGIN) + 110
-                    pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))  # 16.1 меняем цвет ячейки
-                    # в соответствии со значением из словаря COLORS
-
-                    # 14.3 условие для вывода значения на квадрате
-                    if value != 0:
-                        font_w, font_h = text.get_size()  # 14.4 узнаем размер текста
-                        text_x = w + (SIZE_BLOCK - font_w) / 2  # 14.5 координата размещение текста в квадрате по Х
-                        text_y = h + (SIZE_BLOCK - font_h) / 2  # 14.6 координата размещение текста в квадрате по У
-                        screen.blit(text, (text_x, text_y))  # 14.7 размещаем текст на экране по координатам
-
             # 12.3 переносим данные цикла в это условие
             # input()
             empty = get_empty_list(mas)  # 8.1 переменная, которая принимает список пустых ячеек
@@ -88,6 +89,7 @@ while is_zero_in_mas(mas):
             mas = insert_2_or_4(mas, x, y)  # 8.5 присваиваем по этим координатам 2 или 4 в ячейку
             print(f'Заполнен элемент под номером: {random_num}. Координаты: {x}, {y}')
 
+            draw_interface()  # 17.1 вставляем функцию отрисовки интерфейса после добавления нового элемента
             pygame.display.update()  # подвинули внутрь цикла, чтобы обновление только при нажатии клавиши сразу
 
 # ----------------------------- COMMENTS ----------------------------
