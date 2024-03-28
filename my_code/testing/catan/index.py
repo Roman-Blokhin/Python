2f619e3adb9e561b7d34500f860edef50abbb30c
Roman <11-roman@mail.ru>
2024-03-28 17:04:53 +0300

    Catan. Создание фона и смена места отрисовки в коде

--- my_code/testing/catan/index.py
+++ my_code/testing/catan/index.py
@@ -39,6 +39,8 @@ COLORS = {
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Catan')
 
+pretty_print(mas)
+pygame.display.update()
 
 while is_sero_in_mas(mas):
     for event in pygame.event.get():
@@ -48,12 +50,21 @@ while is_sero_in_mas(mas):
 
         elif event.type == pygame.KEYDOWN:
             pygame.draw.rect(screen, WHITE, (TITLE_RECT))
+            font = pygame.font.SysFont('Comic Sans MS', 70)
+            pretty_print(mas)
+
             for row in range(COUNT_BLOCK):
                 for column in range(COUNT_BLOCK):
                     value = mas[row][column]
+                    text = font.render(f'{value}', True, BLACK)
                     x = column * SIZE_BLOCK + MARGIN * (column + 1)
                     y = row * SIZE_BLOCK + MARGIN * (row + 1) + 110
                     pygame.draw.rect(screen, COLORS[value], (x, y, SIZE_BLOCK, SIZE_BLOCK))
+
+                    if value != 0:
+                        font_w, font_h = text.get_size()
+                        text_x = x + (SIZE_BLOCK - font_w) / 2
+                        text_y = y + (SIZE_BLOCK - font_h) / 2
 
             # input()
             empty = get_empty_list(mas)
@@ -63,9 +74,8 @@ while is_sero_in_mas(mas):
             print(f'Выбрано число: {random_number}, с координатами: x = {x}, y = {y}')
             mas = insert_num_to_mas(mas, x, y)
             print(get_empty_list(mas))
-            pretty_print(mas)
 
 
-    pygame.display.update()
+            pygame.display.update()
 
 # 16 = 3 red 5 green 4 sheep 2 grey 2 yellow
