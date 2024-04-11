0daaff759527dfd5b8bd354904918875bcc6ccf2
Roman <11-roman@mail.ru>
2024-04-11 12:02:27 +0300

    6. Pygame. 2048. Создали запрос для вывода 3 игроков с максимальными очками

--- my_code/game_2048/database.py
+++ my_code/game_2048/database.py
@@ -11,11 +11,20 @@ cur = bd.cursor()
 
 # 5. создаем таблицу в базе данных, прописываем команду создания таблицы, ее название
 # указываем название колонок и типы данных для хранения
+# 5.1 if not exists - означает "если таблица еще не создана", прописываем, если будет еще sql запрос далее
 cur.execute("""
-create table RECORDS (  
+create table if not exists RECORDS (  
     name text,
     score integer
 )""")
+
+# 6. создаем новую таблицу с корректным запросом вывода информации
+cur.execute("""
+SELECT name gamer, max(score) score FROM RECORDS
+GROUP by name
+ORDER by score DESC
+LIMIT 3
+""")
 
 # 4. закрываем курсор
 cur.close()
@@ -25,3 +34,6 @@ cur.close()
 # (ROWID - стандартное поле порядка строк, все числа уникальные, при удалении не восстанавливаются)
 # ORDER by score - сортировка по колонке score от меньшего к большему
 # ORDER by score DESC - сортировка по колонке score от большего к меньшему
+# LIMIT 3 - выводит только первые 3 строки после сортировки
+# GROUP by name - группировка по колонке name
+# SELECT name, max(score) FROM RECORDS - max(score) выводит максимальное число по колонке score после группировки
