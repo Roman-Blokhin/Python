b7dff5dc53e290651923ee3907c6bbe6b92bf13f
Roman <11-roman@mail.ru>
2024-05-11 13:31:06 +0300

    Django. Создали файл about.html и прописали путь к нему

--- django/romansite/main/views.py
+++ django/romansite/main/views.py
@@ -4,13 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse  # 1. импортируем для написания текста на странице
 
 
-# Create your views here.
-
 # 2. создаем функцию для вывода шаблона
 def index(request):
     return render(request, 'main/index.html')
 
 
-# 3. создаем метод для страницы about, указываем аргумент обязательно
+# 4. создаем метод для страницы about
 def about(request):
-    return HttpResponse('<h1>Обо мне</h1>')
+    return render(request, 'main/about.html')
+
+
+# 3. создаем метод для страницы about, указываем аргумент обязательно, HttpResponse - устаревшее, лучше render
+# def about(request):
+#     return HttpResponse('<h1>Обо мне</h1>')
