323aaa04663edb01a6f6f325370000813c77bc73
Roman <11-roman@mail.ru>
2024-05-12 18:29:46 +0300

    Django. Подключение локального файла стилей к проекту

--- django/romansite/romansite/settings.py
+++ django/romansite/romansite/settings.py
@@ -132,6 +132,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# УКАЗЫВАЕТ ИЗ ДОКУМЕНТАЦИИ ДЛЯ ТОГО, ЧТОБЫ ЛОКАЛЬНЫЙ ФАЙЛ СТИЛЕЙ .css КОРРЕКТНО РАБОТАЛ
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
