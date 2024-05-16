a947e84827178c0917b6132e062cd71601bf6506
Roman <11-roman@mail.ru>
2024-05-16 19:22:41 +0300

    Django. Создание файла миграций. Проведение миграции

--- django/romansite/news/models.py
+++ django/romansite/news/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Article(models.Model):  # 1. создаем класс для базы данных
     # 2. нужно каждую колонку обозначить типом данных, написать название, добавить параметры
-    title = models.CharField('Title',  max_lenght=50)  # 2.1 строка до 250 символов
-    anons = models.CharField('Notification',  max_lenght=250)
+    title = models.CharField('Title',  max_length=50)  # 2.1 строка до 250 символов
+    anons = models.CharField('Notification',  max_length=250)
     full_text = models.TextField('Article')  # 2.2 строка больше 250 символов
     date = models.DateTimeField('Дата публикации')  # 2.3 дата и время
 
