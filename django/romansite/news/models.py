0676f206716a48a8668d8ddfb902d46443868202
Roman <11-roman@mail.ru>
2024-05-16 14:46:31 +0300

    Django. Пишем класс для создания базы данных. Колонки и типы данных

--- django/romansite/news/models.py
+++ django/romansite/news/models.py
@@ -1,3 +1,12 @@
+# СОЗДАЕМ БАЗУ ДАНННЫХ
+
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):  # 1. создаем класс для базы данных
+    # 2. нужно каждую колонку обозначить типом данных, написать название, добавить параметры
+    title = models.CharField('Title',  max_lenght=50)  # 2.1 строка до 250 символов
+    anons = models.CharField('Notification',  max_lenght=250)
+    full_text = models.TextField('Article')  # 2.2 строка больше 250 символов
+    date = models.DateTimeField('Дата публикации')  # 2.3 дата и время
+
