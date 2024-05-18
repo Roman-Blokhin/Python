738741923f80ed23f77a6575f9ed55cc45c10cce
Roman <11-roman@mail.ru>
2024-05-18 20:57:56 +0300

    Django. Дописываем функцию для вывода таблицы новостей на страницу сайта

--- django/romansite/news/views.py
+++ django/romansite/news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Article  # 2 импортируем табличку Article
 
 def news_home(request):
-    return render (request, 'news/news_home.html')  # 1. Можно использовать шаблоны, которые были в другом приложении.
+    news = Article.objects.all()  # 2.1 переменная, которая принимает на себя все объекты таблицы Article
+    return render (request, 'news/news_home.html', {'news': news})
+    # 1. Можно использовать шаблоны, которые были в другом приложении. 3им параметром передаем news
