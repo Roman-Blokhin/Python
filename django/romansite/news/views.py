a210890b537f4974da96a895712e7d5620988422
Roman <11-roman@mail.ru>
2024-05-20 12:54:17 +0300

    Django. Добавили сортировку для вывода статей на сайт, а также указали срез количества статей

--- django/romansite/news/views.py
+++ django/romansite/news/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Article  # 2 импортируем табличку Article
 
 def news_home(request):
-    news = Article.objects.all()  # 2.1 переменная, которая принимает на себя все объекты таблицы Article
+    # news = Article.objects.all()  # 2.1 переменная, которая принимает на себя все объекты таблицы Article
+    # news = Article.objects.order_by('-title')  # 2.3 сортировка статей по возрастанию
+    # news = Article.objects.order_by('title')  # 2.2 сортировка статей по убыванию
+    news = Article.objects.order_by('-date') [:2]  # 2.2 сортировка по дате публикации, срез - 2 записи
+
     return render (request, 'news/news_home.html', {'news': news})
     # 1. Можно использовать шаблоны, которые были в другом приложении.
     # 2.2 Третьим параметром передаем news в виде ключ: значение
