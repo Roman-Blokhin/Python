0435da8d624a8e558f029219c7b9346deaf12835
Roman <11-roman@mail.ru>
2024-05-30 09:48:06 +0300

    Django new project

--- django/robsite/robsite/settings.py
+++ django/robsite/robsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-us2oq7)ozpevxihzpdlt)3k1p_ae7gg@e6z0hkmzc-diozaupp'
+SECRET_KEY = 'django-insecure-kx)^dq=!pc31q^ms952^dff%sc4$wc(ecfk46*l$a3)1925&6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
