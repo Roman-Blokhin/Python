dd7d76ccfa83698e25df0e32c3987f390631e045
Roman <11-roman@mail.ru>
2024-05-30 21:40:59 +0300

    Django. Обязательная регистрация нового приложения в файле с настройками

--- django/robsite/robsite/settings.py
+++ django/robsite/robsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dogs.apps.DogsConfig'  # 1. всегда регистрируем наше новое приложение
 ]
 
 MIDDLEWARE = [
