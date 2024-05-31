16e522c92d337f0b11a8fbb492cd4fd7523f039f
Roman <11-roman@mail.ru>
2024-05-31 09:21:58 +0300

    Django. Динамические страницы по любым символам - slug

--- django/robsite/dogs/urls.py
+++ django/robsite/dogs/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),  # 1. прописываем путь на нашу функцию отображения страницы, будет главной
-    path('cats/<int:cat_id>/', views.categories),  # 2. делаем динамическую страницу
+    path('cats/<int:cat_id>/', views.categories),  # 2. делаем динамическую страницу по числу
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # 2. делаем динамическую страницу всем символам
 ]
