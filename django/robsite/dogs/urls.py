c9a5682361efc06e6a37dfe9b0898927e0fe5f1b
Roman <11-roman@mail.ru>
2024-05-31 09:38:15 +0300

    Django. Динамический путь через использование регулярных выражений - re_path

--- django/robsite/dogs/urls.py
+++ django/robsite/dogs/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.index),  # 1. прописываем путь на нашу функцию отображения страницы, будет главной
     path('cats/<int:cat_id>/', views.categories),  # 2. делаем динамическую страницу по числу
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # 2. делаем динамическую страницу всем символам
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # 3. делаем динамическую страницу всем символам
+    re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)  # 4. путь через использование регулярных выражений
 ]
