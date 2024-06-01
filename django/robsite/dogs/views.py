a0a22fc0f6bf427800d742bb3e0b34cc021b7131
Roman <11-roman@mail.ru>
2024-06-01 21:38:49 +0300

    Django. Создали обработчик событий, когда страница не найдена

--- django/robsite/dogs/views.py
+++ django/robsite/dogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 def index(request):
@@ -17,4 +17,8 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     return HttpResponse(f'<h1>Архив по годам</h1> <p>{year}</p>')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена =(</h1>')
 
--- django/robsite/robsite/urls.py
+++ django/robsite/robsite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from dogs.views import views
+from dogs.views import page_not_found
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
