80c2fe0d654bb8eb145d22be855a76c6ed4332fb
Roman <11-roman@mail.ru>
2024-06-02 21:46:21 +0300

    Django. Обработка исключений, когда страница не найдена

--- django/robsite/dogs/views.py
+++ django/robsite/dogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
@@ -16,6 +16,8 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2024:
+        raise Http404()
     return HttpResponse(f'<h1>Архив по годам</h1> <p>{year}</p>')
 
 
