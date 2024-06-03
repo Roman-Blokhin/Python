c57ee512e360ddc6b17625d1414e8862ef9bf716
Roman <11-roman@mail.ru>
2024-06-03 19:45:38 +0300

    Django. Прописал имена маршрутов у ссылок

--- django/robsite/dogs/urls.py
+++ django/robsite/dogs/urls.py
@@ -8,8 +8,8 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),  # 1. прописываем путь на нашу функцию отображения страницы, будет главной
-    path('cats/<int:cat_id>/', views.categories),  # 2. делаем динамическую страницу по числу
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # 3. делаем динамическую страницу всем символам
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),  # 2. делаем динамическую страницу по числу
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # 3. делаем динамическую стр. всем символам
     # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)  # 4. путь через использование регулярных выражений
-    path('archive/<year4:year>/', views.archive)  # 5. прямой путь через использование конвертера
+    path('archive/<year4:year>/', views.archive, name='archive')  # 5. прямой путь через использование конвертера
 ]
