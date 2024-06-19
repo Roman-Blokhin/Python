84ce6cc48d20b4936e775c220f0158d58e1a1cf3
Roman <11-roman@mail.ru>
2024-06-19 14:09:41 +0300

    Django. Создание коллекций для отображения меню и категорий меню

--- django/portfoliosite/projects/views.py
+++ django/portfoliosite/projects/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'projects/index.html')
+    data = {
+        'title': 'Главная страница',
+        'categories': categories_db,
+        'menu': menu,
+    }
+    return render(request, 'projects/index.html', context=data)
 
 
 def about(request):
     return render(request, 'projects/about.html')
+
+
+categories_db = [
+    {'id': 1, 'name': 'Служба поддержки'},
+    {'id': 2, 'name': 'Лаборатория инноваций'},
+    {'id': 3, 'name': 'Международный аудит'},
+    {'id': 4, 'name': 'ВкусВилл спорт'},
+    {'id': 5, 'name': 'Обучение AI'},
+]
+
+menu = [
+    {'title': 'ВкусВилл', 'page_name': 'vkusvill'},
+    {'title': 'Слетать.ру', 'page_name': 'sletat'},
+    {'title': 'Мои скилы', 'page_name': 'skills'},
+]
