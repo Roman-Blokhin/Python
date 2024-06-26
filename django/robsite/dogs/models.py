3afbfc59b8aa30de523bd04013a970c0c590da8c
Roman <11-roman@mail.ru>
2024-06-26 22:17:10 +0300

    Django. Создаем класс для создания базы данных

--- django/robsite/dogs/models.py
+++ django/robsite/dogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# 1. Создаем класс для создания базы данных
+class Dogs(model.Model):
+    title = models.CharField(length=255)  # 2. текстовое поле, длина 255 символов
+    content = models.TextField(blank=True)  # 3. большое текстовое поле, необязательно для заполнения(blank)
+    time_create = models.DateTimeField(auto_now_add=True)  # 4. время создания записи, заполняется автоматически
+    time_update = models.DateTimeField(auto_now=True)  # 5. время изменения записи, заполняется автоматически
+    is_published = models.BooleanField(default=True)  # 6. автоматом проставляется, что статья опубликована
