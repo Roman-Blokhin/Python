6d6a433a9a454360345d9f39f684c950f8e242ac
Roman <11-roman@mail.ru>
2024-06-27 09:08:58 +0300

    Django. Провели миграцию, создался файл с таблицей БД

--- django/robsite/dogs/models.py
+++ django/robsite/dogs/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # 1. Создаем класс для создания базы данных
-class Dogs(model.Model):
-    title = models.CharField(length=255)  # 2. текстовое поле, длина 255 символов
+class Dogs(models.Model):
+    title = models.CharField(max_length=255)  # 2. текстовое поле, длина 255 символов
     content = models.TextField(blank=True)  # 3. большое текстовое поле, необязательно для заполнения(blank)
     time_create = models.DateTimeField(auto_now_add=True)  # 4. время создания записи, заполняется автоматически
     time_update = models.DateTimeField(auto_now=True)  # 5. время изменения записи, заполняется автоматически
