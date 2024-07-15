0c840b4a1a137dd9aa3c62b01f587f3055ba10f0
Roman <11-roman@mail.ru>
2024-07-15 16:09:20 +0300

    Игра - собери рюкзак. Добавил новое меню

--- my_code/testing/bagpack/index.py
+++ my_code/testing/bagpack/index.py
@@ -1,8 +1,5 @@
 # каждая вещь имеет цену
 # можно продавать вещи
-# условие заполненности рюкзака
-#
-#
 
 free_bagpack = 0
 fool_bagpack = 2
@@ -11,8 +8,11 @@ gold = 400
 
 sticks = 'Палки'
 gas = 'Газовый баллон'
+fire = 'Горелка'
+dishes = 'Набор посуды'
+lighter = 'Зажигалка'
 
-list = [sticks, gas, 'Горелка', 'Набор посуды', 'Зажигалка']
+list = [sticks, gas, fire, dishes, lighter]
 list_2 = []
 
 # a6 = '6. Спальный мешок'
@@ -24,58 +24,98 @@ list_2 = []
 print('\nУ тебя есть рюкзак. Сейчас он пустой, заполни его!')
 
 while True:
-    for el in range(len(list)):
-        print(list[el])
-    print('\n0. Заработать денег')
+    print('\n1. Магазин')
+    print('2. Заработать денег')
+    print('3. Проверить инвентарь')
+    print('4. Продать вещи')
 
     answer = input('\nЧто ты выберешь: ')
 
-    if answer == 'Палки' or answer == 'палки':
-        if gold == 0:
-            gold += 0
-            print('\nУ вас нет денег, отправляйтесь на работу')
-        elif free_bagpack == fool_bagpack:
-            free_bagpack += 0
-            print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
-        else:
-            list_2.append(sticks)
-            free_bagpack += 1
-            gold -= 100
-            print('Вы выбрали:', sticks)
+    if answer == '1' or answer == 'магазин' or answer == 'Магазин':
+        print('\nТовары:')
+        print(' ')
+        for el in range(len(list)):
+            print(list[el])
 
-    elif answer == 'Газовый баллон' or answer == 'газовый баллон':
-        list_2.append('Газовый баллон')
-        gold -= 100
-        print('Вы выбрали:', gas)
+        answer_1 = input('\nЧто ты выберешь: ')
 
-    elif answer == 'Горелка' or answer == 'горелка':
-        list_2.append('Горелка')
-        gold -= 100
-        print('Вы выбрали:', 'Горелка')
-        if gold <= 0:
-            print('У вас нет денег, отправляйтесь на работу')
+        if answer_1 == 'Палки' or answer_1 == 'палки':
+            if gold == 0:
+                gold += 0
+                print('\nУ вас нет денег, отправляйтесь на работу')
+            elif free_bagpack == fool_bagpack:
+                free_bagpack += 0
+                print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
+            else:
+                list_2.append(sticks)
+                free_bagpack += 1
+                gold -= 100
+                print('Вы выбрали:', sticks)
 
-    elif answer == 'Набор посуды' or answer == 'набор посуды':
-        list_2.append('Набор посуды')
-        gold -= 100
-        print('Вы выбрали:', 'Набор посуды')
-        if gold <= 0:
-            print('У вас нет денег, отправляйтесь на работу')
 
-    elif answer == 'Зажигалка' or answer == 'зажигалка':
-        list_2.append('Зажигалка')
-        gold -= 100
-        print('Вы выбрали:', 'Зажигалка')
-        if gold <= 0:
-            print('У вас нет денег, отправляйтесь на работу')
+        elif answer_1 == 'Газовый баллон' or answer_1 == 'газовый баллон':
+            if gold == 0:
+                gold += 0
+                print('\nУ вас нет денег, отправляйтесь на работу')
+            elif free_bagpack == fool_bagpack:
+                free_bagpack += 0
+                print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
+            else:
+                list_2.append(gas)
+                free_bagpack += 1
+                gold -= 100
+                print('Вы выбрали:', gas)
 
-    elif answer == '0' or answer == 'заработать денег' or answer == 'заработать' or answer == 'Заработать денег':
+
+        elif answer_1 == 'Горелка' or answer_1 == 'горелка':
+            if gold == 0:
+                gold += 0
+                print('\nУ вас нет денег, отправляйтесь на работу')
+            elif free_bagpack == fool_bagpack:
+                free_bagpack += 0
+                print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
+            else:
+                list_2.append(fire)
+                free_bagpack += 1
+                gold -= 100
+                print('Вы выбрали:', fire)
+
+
+        elif answer_1 == 'Набор посуды' or answer_1 == 'набор посуды':
+            if gold == 0:
+                gold += 0
+                print('\nУ вас нет денег, отправляйтесь на работу')
+            elif free_bagpack == fool_bagpack:
+                free_bagpack += 0
+                print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
+            else:
+                list_2.append(dishes)
+                free_bagpack += 1
+                gold -= 100
+                print('Вы выбрали:', dishes)
+
+
+        elif answer_1 == 'Зажигалка' or answer_1 == 'зажигалка':
+            if gold == 0:
+                gold += 0
+                print('\nУ вас нет денег, отправляйтесь на работу')
+            elif free_bagpack == fool_bagpack:
+                free_bagpack += 0
+                print('\nРюкзак заполнен, продайте вещи или купите новый рюкзак')
+            else:
+                list_2.append(lighter)
+                free_bagpack += 1
+                gold -= 100
+                print('Вы выбрали:', lighter)
+
+
+    elif answer == '1' or answer == 'заработать денег' or answer == 'заработать' or answer == 'Заработать денег':
         gold += 10
         print(f'Вы заработали 10 золотых, баланс: {gold} ')
 
 
     print('\nИнвентарь:', ', '.join(list_2), '\nРюкзак:', free_bagpack, '/', fool_bagpack, f'\nДеньги: {gold}')
-    input('\nДля продолжения покупок нажмите Enter')
+    input('\nДля продолжения нажми Enter')
 
     if len(list_2) == 5:
         print('Рюкзак заполнен')
