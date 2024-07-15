3dc7a7c508dc3fd63ea70b21554752082e5bda78
Roman <11-roman@mail.ru>
2024-07-15 19:59:45 +0300

    Игра - собери рюкзак. Добавил меню зарабатывания денег и инвентаря

--- my_code/testing/bagpack/index.py
+++ my_code/testing/bagpack/index.py
@@ -14,6 +14,14 @@ lighter = 'Зажигалка'
 list = [sticks, gas, fire, dishes, lighter]
 list_2 = []
 
+
+def inventory():
+    print('\nИнвентарь:', Fore.YELLOW + ', '.join(list_2) + Style.RESET_ALL,
+          '\nРюкзак:', free_bagpack, '/', fool_bagpack,
+          f'\nДеньги: {gold}')
+    input(Fore.BLUE + '\nДля продолжения нажми Enter ' + Style.RESET_ALL)
+
+
 print(Fore.RED + '\nУ тебя есть рюкзак. Сейчас он пустой, заполни его!' + Style.RESET_ALL)
 
 while True:
@@ -32,6 +40,7 @@ while True:
 
         answer_1 = input(Fore.GREEN + '\nЧто ты выберешь: ' + Style.RESET_ALL)
 
+# ----------------------------------------------- МЕНЮ - МАГАЗИН -----------------------------------------------
         if answer_1 == 'Палки' or answer_1 == 'палки':
             if gold == 0:
                 gold += 0
@@ -44,6 +53,7 @@ while True:
                 free_bagpack += 1
                 gold -= 100
                 print('Вы выбрали:', Fore.RED + sticks + Style.RESET_ALL)
+                inventory()
 
 
         elif answer_1 == 'Газовый баллон' or answer_1 == 'газовый баллон':
@@ -58,6 +68,7 @@ while True:
                 free_bagpack += 1
                 gold -= 100
                 print('Вы выбрали:', Fore.RED + gas + Style.RESET_ALL)
+                inventory()
 
 
         elif answer_1 == 'Горелка' or answer_1 == 'горелка':
@@ -72,6 +83,7 @@ while True:
                 free_bagpack += 1
                 gold -= 100
                 print('Вы выбрали:', Fore.RED + fire + Style.RESET_ALL)
+                inventory()
 
 
         elif answer_1 == 'Набор посуды' or answer_1 == 'набор посуды':
@@ -86,6 +98,7 @@ while True:
                 free_bagpack += 1
                 gold -= 100
                 print('Вы выбрали:', Fore.RED + dishes + Style.RESET_ALL)
+                inventory()
 
 
         elif answer_1 == 'Зажигалка' or answer_1 == 'зажигалка':
@@ -100,17 +113,21 @@ while True:
                 free_bagpack += 1
                 gold -= 100
                 print('Вы выбрали:', Fore.RED + lighter + Style.RESET_ALL)
+                inventory()
 
+# --------------------------------------- МЕНЮ - ЗАРАБОТАТЬ ДЕНЕГ ---------------------------------------
 
-    elif answer == '1' or answer == 'заработать денег' or answer == 'заработать' or answer == 'Заработать денег':
+    elif answer == '2' or answer == 'заработать денег' or answer == 'заработать' or answer == 'Заработать денег':
         gold += 10
-        print(Fore.RED + f'Вы заработали 10 золотых, баланс: {gold} ' + Style.RESET_ALL)
+        print(Fore.RED + '\n' + f'Вы заработали 10 золотых, баланс: {gold} ' + Style.RESET_ALL)
+
+# --------------------------------------- МЕНЮ - ПРОВЕРИТЬ ИНВЕНТАРЬ ---------------------------------------
+    elif answer == '3' or answer == 'инвентарь' or answer == 'проверить инвентарь' or answer == 'проверить инвентарь':
+        inventory()
+
+# --------------------------------------- МЕНЮ - ПРОДАТЬ ВЕЩИ ---------------------------------------
 
 
-    print('\nИнвентарь:', Fore.YELLOW + ', '.join(list_2) + Style.RESET_ALL,
-          '\nРюкзак:', free_bagpack, '/', fool_bagpack,
-          f'\nДеньги: {gold}')
-    input(Fore.BLUE + '\nДля продолжения нажми Enter ' + Style.RESET_ALL)
 
     if len(list_2) == 5:
         print(Fore.RED + 'Рюкзак заполнен' + Style.RESET_ALL)
