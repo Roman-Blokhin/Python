682e9fcdd79c6172cb4669070816d0c84da4703c
Roman <11-roman@mail.ru>
2024-07-17 13:11:59 +0300

    Игра - собери рюкзак GUI. Функция для кнопки - Горелка

--- my_code/bagpack_gui/main.py
+++ my_code/bagpack_gui/main.py
@@ -113,6 +113,34 @@ def karemat_btn():  # кнопка - КАРЕМАТ
             print(full_bag)
 
 
+
+def fire_btn():  # кнопка - ГОРЕЛКА
+    global gold, list_inventory, min_bag, max_bag
+    if gold < fire_cost:
+        gold += 0
+        lbl_11.config(text=gold)
+        lbl_15 = Label(tab_1, text=no_money, font=('Arial', 13, 'normal'), fg='red')
+        lbl_15.grid(row=12, column=0, columnspan=4)
+        print(no_money)
+    else:
+        if min_bag < max_bag:
+            gold -= fire_cost
+            lbl_11.config(text=gold)
+            print('Деньги:', gold)
+            list_inventory.append(fire)
+            lbl_14.config(text=list_inventory)
+            print('Инвентарь:', list_inventory)
+            min_bag += 1
+            lbl_12.config(text=(min_bag, '/', max_bag))
+            print('Товаров в рюкзаке:', min_bag, '/', max_bag)
+        else:
+            min_bag += 0
+            gold += 0
+            lbl_16 = Label(tab_1, text=full_bag, font=('Arial', 13, 'normal'), fg='red')
+            lbl_16.grid(row=13, column=0, columnspan=4)
+            print(full_bag)
+
+
 def level_up():
     global gold, level
     if gold >= level_bagpack_cost:
@@ -224,7 +252,7 @@ btn_4 = Button(tab_1, text='Каремат', font=('Arial', 13, 'normal'), width
 btn_4.grid(row=4, column=3, sticky='swen', padx=3, pady=3)
 
 # кнопка - ГОРЕЛКА
-btn_5 = Button(tab_1, text='Горелка', font=('Arial', 13, 'normal'), width=9)
+btn_5 = Button(tab_1, text='Горелка', font=('Arial', 13, 'normal'), width=9, command=fire_btn)
 btn_5.grid(row=5, column=0, sticky='swen', padx=3, pady=3)
 
 # кнопка - ПОСУДА
