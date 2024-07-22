2c77858124d4aae1a56e907a3d2293279999b480
Roman <11-roman@mail.ru>
2024-07-22 21:49:12 +0300

    FLET. Создание приложения - кликер. Добавлена строка с элементами

--- ui_apps/flet_clicker.py
+++ ui_apps/flet_clicker.py
@@ -8,21 +8,28 @@ def main(page: flet.Page):
     text_label = flet.TextField('0', text_align=flet.TextAlign.CENTER, width=100)
 
     def click_remove(event):
-        text_label.value = int(text_label.value) - 1
+        text_label.value = str(int(text_label.value) - 1) 
         page.update()
 
 
     def click_plus(event):
-        text_label.value = int(text_label.value) + 1
+        text_label.value = str(int(text_label.value) + 1) 
         page.update()
 
     page.add(
         flet.Row(
             [
+                flet.Icon(flet.icons.DANGEROUS_SHARP),
+                flet.Text('Давай считать твои клики', color='red'),
+            ],
+            alignment=flet.MainAxisAlignment.CENTER
+        ),
+
+        flet.Row(
+            [
                 flet.IconButton(flet.icons.REMOVE, on_click=click_remove),
                 text_label,
-                flet.IconButton(flet.icons.ADD, on_click=click_plus)
-
+                flet.IconButton(flet.icons.ADD, on_click=click_plus),
             ],
             alignment=flet.MainAxisAlignment.CENTER
         )
