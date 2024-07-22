3b2b338b108e9510a3acd5ec76c4d69fb8328d6e
Roman <11-roman@mail.ru>
2024-07-22 15:26:00 +0300

    FLET. Добавление элементов на экран приложения

--- ui_apps/index_flet.py
+++ ui_apps/index_flet.py
@@ -2,6 +2,18 @@ import flet as ft
 
 def main(page: ft.Page):  # Основная функция + ft.Page - включает подсказки
     page.title = 'Flet App'  # заголовок
-    page.theme_mode = 'dark'  # тема
+    page.theme_mode = 'light'  # тема
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER  # вертикальное расположение элементов
 
-ft.app(target=main)  # прописываем какая функция будет запускаться при старте проект
+    # добавляет на экран элементы 
+    page.add(  
+        ft.Row(  # добавляет строки
+            [
+                ft.IconButton(ft.icons.HOME),  # добавили кнопку - иконку домика
+                ft.Icon(ft.icons.BACK_HAND)  # добавили иконку - рука
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+ft.app(target=main)  # прописываем какая функция будет запускаться при старте проектр
