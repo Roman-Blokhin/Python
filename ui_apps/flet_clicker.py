ce8a4f774bb2ae896cf186b605d8f1e0fdc9cf37
Roman <11-roman@mail.ru>
2024-07-22 21:34:20 +0300

    FLET. Создание приложения - кликер. Функции вычитания и сложения + отображение результата на экране

--- ui_apps/flet_clicker.py
+++ ui_apps/flet_clicker.py
@@ -2,23 +2,30 @@ import flet
 
 def main(page: flet.Page):
     page.title = 'Кликер'
-    page.theme_mode = 'light'
+    page.theme_mode = 'dark'
     page.vertical_alignment = flet.MainAxisAlignment.CENTER
 
-    text_number = flet.ElevatedButton('Click me')
-    text_label = flet.Text('0', width=10, color='#fgfgfg')
-    page.update()
+    text_label = flet.TextField('0', text_align=flet.TextAlign.CENTER, width=100)
 
-    def click(enter):
-        pass
+    def click_remove(event):
+        text_label.value = int(text_label.value) - 1
+        page.update()
 
-        page.add(
-            flet.Row(
-                [
-                    text_number,
-                    text_label
-                ]
-            )
+
+    def click_plus(event):
+        text_label.value = int(text_label.value) + 1
+        page.update()
+
+    page.add(
+        flet.Row(
+            [
+                flet.IconButton(flet.icons.REMOVE, on_click=click_remove),
+                text_label,
+                flet.IconButton(flet.icons.ADD, on_click=click_plus)
+
+            ],
+            alignment=flet.MainAxisAlignment.CENTER
         )
+    )
     
-    flet.app(target=main)
+flet.app(target=main)
