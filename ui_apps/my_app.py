0d8baa6dc40901410c8977dbbddad02e2c9a7f52
Roman <11-roman@mail.ru>
2024-07-23 18:24:04 +0300

    FLET. Создание приложения - Блокнот. Функция получения данных из чекбатон

--- ui_apps/my_app.py
+++ ui_apps/my_app.py
@@ -4,6 +4,8 @@ def main(page: ft.Page):
     page.title = 'Мой первый проект'
     page.theme_mode = 'light'
 
+    # ---------------------------------------- Переменные --------------------------------------------- #
+
     text_name_label = ft.Text(value='Введите имя: ', color='red')
     text_age_label = ft.Text(value='Введите возраст: ', color='red')
     text_pass_label = ft.Text(value='Введите пароль: ', color='red')
@@ -27,26 +29,51 @@ def main(page: ft.Page):
     gender_label_text = ft.Text('Ваш пол: ')
     gender_label = ft.Text('')
 
+    check_m = ft.Checkbox(value=False)
+    check_f = ft.Checkbox(value=False)
+
+    results = []
+
+    # ------------------------------------- Функции ------------------------------------------------ #
 
     def take_info(event):
         name_label.value = name_enter.value
         age_label.value = age_enter.value
         pass_label.value = pass_enter.value
         # name_enter.focus()  # устанавливает фокус на поле ввода
+        checkbutton_clicked(event)  # срабатывает функция записи результата чекбатона
+        gender_label.value = ''.join(results)  # присваивает значение чекбатон и выводит без скобок и кавычек
+        name_enter.disabled = True  # блокирует поле после записи данных
+        pass_enter.disabled = True
+        age_enter.disabled = True
+        
         page.update()
 
+    def checkbutton_clicked(event):  # функция, создает коллекцию, в нее записывает результат чекбатон и блокирует другой чекбатон
+        if check_m.value:
+            results.append('М')
+            check_f.disabled = True
+        if check_f.value:
+            results.append('Ж')
+            check_m.disabled = True
+
+        if not results:
+            results.append('Не выбрано')
+
+        
+
+    # -------------------------------------- Вывод на экран ----------------------------------------------- #
 
     page.add(
         ft.Row([text_name_label, name_enter, name_label]),
         ft.Row([text_age_label, age_enter, age_label]),
         ft.Row([text_pass_label, pass_enter, pass_label]),
-        ft.Row([ft.Text(value='Выберите пол: ', color='red'), ft.Text(value='М'), ft.Checkbox(value=False), 
-                ft.Text(value='Ж'), ft.Checkbox(value=False)]),
-        ft.Row([ft.ElevatedButton('Отправить', on_click=take_info),]),
-        ft.Row([name_label_text, name_label,]),
+        ft.Row([ft.Text(value='Выберите пол: ', color='red'), ft.Text(value='М'), check_m, ft.Text(value='Ж'), check_f]),
+        ft.Row([ft.ElevatedButton('Отправить', on_click=take_info)]),
+        ft.Row([name_label_text, name_label]),
         ft.Row([age_label_text, age_label]),
         ft.Row([pass_label_text, pass_label]),
-        ft.Row([gender_label_text, gender_label,])
+        ft.Row([gender_label_text, gender_label])
     )
 
 ft.app(target=main)
