55018ca9b2fcb145f74b150286c2a787cd5854f2
Roman <11-roman@mail.ru>
2024-07-23 15:43:45 +0300

    FLET. Создание приложения - Блокнот. форма авторизации и отправки данных

--- ui_apps/my_app.py
+++ ui_apps/my_app.py
@@ -4,38 +4,51 @@ def main(page: ft.Page):
     page.title = 'Мой первый проект'
     page.theme_mode = 'dark'
 
-    text_label = ft.Text(value='Введите имя', color='green')
+    text_name_label = ft.Text(value='Введите имя: ', color='green')
     name_enter = ft.TextField('')
     name_label_text = ft.Text('Ваше имя: ')
     name_label = ft.Text('')
+
+    text_age_label = ft.Text(value='Введите возраст: ', color='green')
+    age_enter = ft.TextField('')
+    age_label_text = ft.Text('Ваше имя: ')
+    age_label = ft.Text('')
 
     def take_name(event):
         name_label.value = name_enter.value
         page.update()
 
+    def take_age(event):
+        age_label.value = age_enter.value
+        page.update()
+
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.MESSAGE),
-                text_label,
+                text_name_label,
                 name_enter,
-                ft.TextButton('Записать имя', on_click=take_name),
                 name_label_text,
                 name_label
             ]
         ),
         ft.Row(
             [
-                ft.IconButton(ft.icons.ARCHIVE),
-                ft.Text(value='Архив', color='green')
+                text_age_label,
+                age_enter,
+                age_label_text,
+                age_label
             ]
         ),
         ft.Row(
             [
-                ft.IconButton(ft.icons.CALENDAR_MONTH),
-                ft.Text('Календарь', color='green')
+                ft.Text(value='Выберите пол: '),
+                ft.Text(value='М'),
+                ft.Checkbox(value=False),
+                ft.Text(value='Ж'),
+                ft.Checkbox(value=False)
+
             ]
-        )
+        ),
     )
 
 ft.app(target=main)
