9d9610a1968780ff65bb6e7dddf280f987925d67
Roman <11-roman@mail.ru>
2024-07-23 17:48:53 +0300

    FLET. Создание приложения - Блокнот. Поле для ввода и вывода пароля

--- ui_apps/my_app.py
+++ ui_apps/my_app.py
@@ -2,19 +2,27 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Мой первый проект'
-    page.theme_mode = 'dark'
+    page.theme_mode = 'light'
 
     text_name_label = ft.Text(value='Введите имя: ', color='red')
     text_age_label = ft.Text(value='Введите возраст: ', color='red')
+    text_pass_label = ft.Text(value='Введите пароль: ', color='red')
 
-    name_enter = ft.TextField(hint_text='Введите текст', text_align=ft.TextAlign.RIGHT)  # hint_text - пример текста
-    age_enter = ft.TextField(label='Введите текст', text_align=ft.TextAlign.RIGHT)  # label - красивая анимация надписи
+    name_enter = ft.TextField(hint_text='Введите текст', text_align=ft.TextAlign.RIGHT, text_size=18)  
+    pass_enter = ft.TextField(label='Пароль', text_align=ft.TextAlign.RIGHT, password=True)  
+    age_enter = ft.TextField(label='Введите текст', text_align=ft.TextAlign.RIGHT)  
+
+    # hint_text - пример текста
+    # label - красивая анимация надписи
     
     name_label_text = ft.Text('Ваше имя: ')
     name_label = ft.Text('')
 
     age_label_text = ft.Text('Ваш возраст: ')
     age_label = ft.Text('')
+
+    pass_label_text = ft.Text('Ваш пароль: ')
+    pass_label = ft.Text('')
 
     gender_label_text = ft.Text('Ваш пол: ')
     gender_label = ft.Text('')
@@ -23,6 +31,7 @@ def main(page: ft.Page):
     def take_info(event):
         name_label.value = name_enter.value
         age_label.value = age_enter.value
+        pass_label.value = pass_enter.value
         # name_enter.focus()  # устанавливает фокус на поле ввода
         page.update()
 
@@ -40,6 +49,13 @@ def main(page: ft.Page):
                 text_age_label,
                 age_enter,
                 age_label
+            ]
+        ),
+        ft.Row(
+            [
+                text_pass_label,
+                pass_enter,
+                pass_label
             ]
         ),
         ft.Row(
@@ -66,6 +82,12 @@ def main(page: ft.Page):
             [
                 age_label_text,
                 age_label
+            ]
+        ),
+        ft.Row(
+            [
+                pass_label_text,
+                pass_label
             ]
         ),
         ft.Row(
