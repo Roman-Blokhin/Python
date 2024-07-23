b2b4d979a1475e9f7d6218528ed6bea401a594f8
Roman <11-roman@mail.ru>
2024-07-23 15:07:43 +0300

    FLET. Создание приложения - Блокнот. Элементы: кнопки и текстовые поля + подключение функции

--- ui_apps/my_app.py
+++ ui_apps/my_app.py
@@ -4,23 +4,36 @@ def main(page: ft.Page):
     page.title = 'Мой первый проект'
     page.theme_mode = 'dark'
 
+    text_label = ft.Text(value='Введите имя', color='green')
+    name_enter = ft.TextField('')
+    name_label_text = ft.Text('Ваше имя: ')
+    name_label = ft.Text('')
+
+    def take_name(event):
+        name_label.value = name_enter.value
+        page.update()
+
     page.add(
         ft.Row(
             [
                 ft.IconButton(ft.icons.MESSAGE),
-                ft.Text('Создать запись')
+                text_label,
+                name_enter,
+                ft.TextButton('Записать имя', on_click=take_name),
+                name_label_text,
+                name_label
             ]
         ),
         ft.Row(
             [
                 ft.IconButton(ft.icons.ARCHIVE),
-                ft.Text('Архив')
+                ft.Text(value='Архив', color='green')
             ]
         ),
         ft.Row(
             [
                 ft.IconButton(ft.icons.CALENDAR_MONTH),
-                ft.Text('Календарь')
+                ft.Text('Календарь', color='green')
             ]
         )
     )
