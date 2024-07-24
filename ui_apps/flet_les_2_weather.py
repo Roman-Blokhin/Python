daad100cca6144ac9856a2a7a63946a3fc554556
Roman <11-roman@mail.ru>
2024-07-24 19:38:01 +0300

    FLET. Приложение - Погода. Функция изменения темы приложения

--- ui_apps/flet_les_2_weather.py
+++ ui_apps/flet_les_2_weather.py
@@ -5,5 +5,21 @@ def main(page: ft.Page):
     page.theme_mode = 'dark'  
     page.vertical_alignment = ft.MainAxisAlignment.CENTER  
 
+    def weather_info(event):
+        pass
+
+    def change_theme(e):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
+        page.update()
+
+
+    user_data = ft.TextField(value='Введите город', width=100)
+    btn_theme = ft.IconButton(ft.icons.SUNNY, on_click=change_theme)
+    text_change_theme = ft.Text('Погодное приложение')
+
+    page.add(
+        ft.Row([btn_theme, text_change_theme], alignment=ft.MainAxisAlignment.CENTER)
+    )
+
 
 ft.app(target=main)  
