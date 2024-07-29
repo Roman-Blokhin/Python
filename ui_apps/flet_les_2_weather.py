e449ce3784618d7e4ec73ed308c23df7118dfdd4
Roman <11-roman@mail.ru>
2024-07-29 09:39:52 +0300

    FLET. Приложение - Погода. Переменные со ссылкой на сервис погоды и ключ

--- ui_apps/flet_les_2_weather.py
+++ ui_apps/flet_les_2_weather.py
@@ -5,18 +5,20 @@ def main(page: ft.Page):
     page.theme_mode = 'dark'  
     page.vertical_alignment = ft.MainAxisAlignment.CENTER  
 
-    def weather_info(event):
-        pass
 
     def get_info(event):
-        pass
+        if len(user_data.value) <= 2:
+            return
+
+        API = 'af76dfe719bf598fba88c3b95130f5d0'
+        URL = f'https://api.openweathermap.org/data/2.5/weather?q={user_data.value}&appid={API}'
 
     def change_theme(e):
         page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
         page.update()
 
 
-    user_data = ft.TextField(value='Введите город', width=200)
+    user_data = ft.TextField(label='Введите город', width=200)
     btn_theme = ft.IconButton(ft.icons.SUNNY, on_click=change_theme)
     text_change_theme = ft.Text('Погодное приложение')
     btn_weather = ft.IconButton(ft.icons.PLAY_ARROW_SHARP, on_click=get_info)
