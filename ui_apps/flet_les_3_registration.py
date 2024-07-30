05dc4c3c3681053ebed907a56a8c1384fb7968d4
Roman <11-roman@mail.ru>
2024-07-30 08:54:32 +0300

    FLET. Форма регистрации с базой данных

---
+++ ui_apps/flet_les_3_registration.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'Регистрация'
+    page.theme_mode = 'light'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window_width = '500'
+    page.window_height = '500'
+    page.window_left = '800'
+    page.window_top = '200'
+    
+    def info(event):
+        page.update()
+        pass
+
+    page.add(
+        ft.Row([]),
+        ft.Row([]),
+    )
+
+ft.app(target=main)
