7859d521b8dda599c982c3cbcf2eec6e3fccc9ec
Roman <11-roman@mail.ru>
2024-07-31 19:08:28 +0300

    Flet. Форма регистрации с базой данных. Создана форма обратной связи с графическим дизайном, учитывая колонку

--- ui_apps/flet_les_3_registration.py
+++ ui_apps/flet_les_3_registration.py
@@ -15,21 +15,32 @@ def main(page: ft.Page):
         pass
         
 
-    def validate(event):
+    def validate(event):  # условие об активации кнопки Отправить
         if all([user_name.value, user_pass.value]):
             btn_reg.disabled = False
+        else:
+            btn_reg.disabled = True
         page.update()
 
 
-    user_name = ft.TextField(label='Введите логин', width=200)
-    user_pass = ft.TextField(label='Введите пароль', width=200, password=True)
+    user_name = ft.TextField(label='Введите логин', width=200, on_change=validate)  # on_change - проверят заполненность полей
+    user_pass = ft.TextField(label='Введите пароль', width=200, password=True, on_change=validate)
     btn_reg = ft.OutlinedButton(text='Отправить', disabled=True, on_click=register)
 
 
     page.add(
-        ft.Row([user_name], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([user_pass], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([btn_reg], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row(
+            [
+                ft.Column(
+                    [
+                        ft.Text('Регистрация:'),
+                        user_name,
+                        user_pass,
+                        btn_reg
+                    ]
+                )
+            ], alignment=ft.MainAxisAlignment.CENTER
+        )
     )
 
 
